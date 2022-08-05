2408c3a7e2495ab43d3dd0549e5bc88a11647fa2
mirtyl-wacdec <ycl2@protonmail.com>
2022-08-05 21:12:46 +0900

    moved paima engine to its own repo

---
+++ runtime/src/index.ts
@@ -0,0 +1,67 @@
+import type {
+  ChainFunnel,
+  GameStateMachine,
+  PaimaRuntimeInitializer,
+  ChainData
+} from "paima-utils";
+import * as fs from "fs/promises";
+import { server, startServer } from "./server.js"
+
+
+const paimaEngine: PaimaRuntimeInitializer = {
+  initialize(chainFunnel, gameStateMachine, gameBackendVersion) {
+    return {
+      pollingRate: 4,
+      chainDataExtensions: [],
+      addGET(route, callback) {
+        server.get(route, callback)
+      },
+      addPOST(route, callback) {
+        server.post(route, callback)
+      },
+      setPollingRate(seconds: number) {
+        this.pollingRate = seconds
+      },
+      addExtensions(chainDataExtensions) {
+        this.chainDataExtensions = [...this.chainDataExtensions, ...chainDataExtensions]
+      },
+      async run() {
+        this.addGET("/backend_version", async (req, res) => {
+          res.status(200).json(gameBackendVersion);
+        });
+        // pass endpoints to web server and run
+        (async () => startServer())();
+        runIterativeFunnel(gameStateMachine, chainFunnel, this.pollingRate);
+      }
+    }
+  }
+}
+async function runIterativeFunnel(gameStateMachine: GameStateMachine, chainFunnel: ChainFunnel, pollingRate: number) {
+  while (true) {
+    const latestReadBlockHeight = await gameStateMachine.latestBlockHeight();
+    console.log(latestReadBlockHeight, "latest read blockheight")
+    const latestChainData = await chainFunnel.readData(latestReadBlockHeight + 1) as ChainData[];
+    if (!latestChainData || !latestChainData?.length) await delay(pollingRate * 1000);
+    else
+      for (let block of latestChainData) {
+        const s1 = `${Date.now()} - ${block.blockNumber} block read, containing ${block.submittedData.length} pieces of input\n`
+        await fs.appendFile("./logs.log", s1)
+        if (block.submittedData.length) console.log(block, "block of chain data being processed")
+        try {
+          await gameStateMachine.process(block);
+          const s2 = `${Date.now()} - ${block.blockNumber} OK\n`
+          await fs.appendFile("./logs.log", s2)
+          // await delay(pollingRate * 1000);
+        }
+        catch (error) {
+          const s3 = `***ERROR***\n${error}\n***\n`;
+          await fs.appendFile("./logs.log", s3)
+        }
+      }
+  }
+}
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export default paimaEngine
