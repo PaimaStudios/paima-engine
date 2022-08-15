adc57c7210ad04ddba948ac150486d44d357472a
mirtyl-wacdec <ycl2@protonmail.com>
2022-08-15 18:03:43 +0200

    added automated db snapshots to paima-runtime

--- runtime/src/index.ts
+++ runtime/src/index.ts
@@ -5,11 +5,14 @@ import type {
   ChainData
 } from "paima-utils";
 import * as fs from "fs/promises";
+import { exec } from "child_process";
 import { server, startServer } from "./server.js"
+const SNAPSHOT_INTERVAL = 151200;
 
 
 const paimaEngine: PaimaRuntimeInitializer = {
   initialize(chainFunnel, gameStateMachine, gameBackendVersion) {
+    // initialize snapshot folder
     return {
       pollingRate: 4,
       chainDataExtensions: [],
@@ -26,20 +29,44 @@ const paimaEngine: PaimaRuntimeInitializer = {
         this.chainDataExtensions = [...this.chainDataExtensions, ...chainDataExtensions]
       },
       async run() {
+        const nextSnapshot = await snapshots();
         this.addGET("/backend_version", async (req, res) => {
           res.status(200).json(gameBackendVersion);
         });
         // pass endpoints to web server and run
         (async () => startServer())();
-        runIterativeFunnel(gameStateMachine, chainFunnel, this.pollingRate);
+        runIterativeFunnel(gameStateMachine, chainFunnel, this.pollingRate, nextSnapshot);
       }
     }
   }
 }
-async function runIterativeFunnel(gameStateMachine: GameStateMachine, chainFunnel: ChainFunnel, pollingRate: number) {
+
+async function snapshots() {
+  const dir = "snapshots"
+  try {
+    const files = await fs.readdir(dir);
+    if (files.length === 0) return SNAPSHOT_INTERVAL
+    const stats = files.map(async (f) => {
+      const s = await fs.stat(dir + "/" + f);
+      return {name: f, stats: s}
+    })
+    const ss = await Promise.all(stats);
+    ss.sort((a, b) => a.stats.mtime.getTime() - b.stats.mtime.getTime())
+    if (ss.length > 2) await fs.rm(dir + "/" + ss[0].name);
+    const maxnum = ss[ss.length -1].name.match(/\d/);
+    const max = maxnum?.[0] || "0"
+    return parseInt(max) + SNAPSHOT_INTERVAL
+  } catch {
+    await fs.mkdir(dir);
+    return SNAPSHOT_INTERVAL
+  }
+}
+async function runIterativeFunnel(gameStateMachine: GameStateMachine, chainFunnel: ChainFunnel, pollingRate: number, snapshotTrigger: number) {
   while (true) {
     const latestReadBlockHeight = await gameStateMachine.latestBlockHeight();
     console.log(latestReadBlockHeight, "latest read blockheight")
+    // take DB snapshot first
+    if (latestReadBlockHeight === snapshotTrigger) await saveSnapshot(latestReadBlockHeight);
     const latestChainData = await chainFunnel.readData(latestReadBlockHeight + 1) as ChainData[];
     if (!latestChainData || !latestChainData?.length) await delay(pollingRate * 1000);
     else
@@ -62,6 +89,13 @@ async function runIterativeFunnel(gameStateMachine: GameStateMachine, chainFunne
 }
 function delay(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function saveSnapshot(blockHeight: number) {
+  const username = process.env.DB_USER;
+  const database = process.env.DB_NAME;
+  const fileName = `paima-snapshot-${blockHeight}.tar`;
+  exec(`pg_dump -U ${username} -d ${database} -f ./snapshots/${fileName} -F t`,)
 }
 
 export default paimaEngine
