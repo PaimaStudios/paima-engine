24b9a41d2db5a77e5ee9c5bdc0ea283d523402ae
Martin Tomko <martin.tomko@vacuumlabs.com>
2022-09-26 14:52:02 +0200

    Ran lint

--- paima-sm/src/randomness.ts
+++ paima-sm/src/randomness.ts
@@ -1,21 +1,26 @@
-import pg from "pg";
 import Crypto from "crypto";
-
+import pg from "pg";
 
 import { getRandomness } from "./sql/queries.queries.js";
 
 export function randomnessRouter(n: number) {
-  if (n) return getSeed1
-  else throw Error("wrong randomness protocol set")
+  if (n) return getSeed1;
+  else throw Error("wrong randomness protocol set");
 }
 
 // Basic randomness generation protocol which hashes together previous seeds + latest block hash
-async function getSeed1(latestChainData: any, DBConn: pg.Pool): Promise<string> {
+async function getSeed1(
+  latestChainData: any,
+  DBConn: pg.Pool
+): Promise<string> {
   const hashes = await getRandomness.run(undefined, DBConn);
-  const seed = hashTogether([latestChainData.blockHash, ...hashes.map(h => h.seed)]);
-  return seed
+  const seed = hashTogether([
+    latestChainData.blockHash,
+    ...hashes.map((h) => h.seed),
+  ]);
+  return seed;
 }
 
 function hashTogether(hashes: string[]): string {
-  return Crypto.createHash('sha256').update(hashes.join()).digest('base64');
+  return Crypto.createHash("sha256").update(hashes.join()).digest("base64");
 }
