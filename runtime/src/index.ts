28b389de54109fe0a4760ff3e7acdc14f2cb6279
Martin Tomko <martin.tomko@vacuumlabs.com>
2022-10-07 11:53:52 +0200

    Ran lint

--- runtime/src/index.ts
+++ runtime/src/index.ts
@@ -1,133 +1,164 @@
-import type {
-  ChainFunnel,
-  GameStateMachine,
-  PaimaRuntimeInitializer,
-  ChainData,
-} from "paima-utils";
-import { doLog, logBlock, logSuccess, logError } from "paima-utils";
 import * as fs from "fs/promises";
-import { server, startServer } from "./server.js"
+import type {
+    ChainData,
+    ChainFunnel,
+    GameStateMachine,
+    PaimaRuntimeInitializer,
+} from "paima-utils";
+import { doLog, logError, logSuccess } from "paima-utils";
 import process from "process";
+import { server, startServer } from "./server.js";
 import { snapshotIfTime } from "./snapshots.js";
 let run = true;
 
 process.on("SIGINT", () => {
-  doLog("Caught SIGINT. Waiting for engine to finish processing current block");
-  run = false;
+    doLog(
+        "Caught SIGINT. Waiting for engine to finish processing current block"
+    );
+    run = false;
 });
 
 process.on("SIGTERM", () => {
-  doLog("Caught SIGTERM. Waiting for engine to finish processing current block");
-  run = false;
+    doLog(
+        "Caught SIGTERM. Waiting for engine to finish processing current block"
+    );
+    run = false;
 });
 
-process.on('exit', (code) => {
-  doLog(`Exiting with code: ${code}`);
+process.on("exit", code => {
+    doLog(`Exiting with code: ${code}`);
 });
-
 
 const paimaEngine: PaimaRuntimeInitializer = {
-  initialize(chainFunnel, gameStateMachine, gameBackendVersion) {
-    // initialize snapshot folder
-    return {
-      pollingRate: 4,
-      chainDataExtensions: [],
-      addGET(route, callback) {
-        server.get(route, callback)
-      },
-      addPOST(route, callback) {
-        server.post(route, callback)
-      },
-      setPollingRate(seconds: number) {
-        this.pollingRate = seconds
-      },
-      addExtensions(chainDataExtensions) {
-        this.chainDataExtensions = [...this.chainDataExtensions, ...chainDataExtensions]
-      },
-      async run() {
-        const finalBlockHeight = await getFinalBlockHeight();
-        doLog(`Final block height set to ${finalBlockHeight}`);
-        this.addGET("/backend_version", async (req, res) => {
-          res.status(200).json(gameBackendVersion);
-        });
-        // pass endpoints to web server and run
-        (async () => startServer())();
-        runIterativeFunnel(gameStateMachine, chainFunnel, this.pollingRate, finalBlockHeight);
-      }
-    }
-  }
-}
+    initialize(chainFunnel, gameStateMachine, gameBackendVersion) {
+        // initialize snapshot folder
+        return {
+            pollingRate: 4,
+            chainDataExtensions: [],
+            addGET(route, callback) {
+                server.get(route, callback);
+            },
+            addPOST(route, callback) {
+                server.post(route, callback);
+            },
+            setPollingRate(seconds: number) {
+                this.pollingRate = seconds;
+            },
+            addExtensions(chainDataExtensions) {
+                this.chainDataExtensions = [
+                    ...this.chainDataExtensions,
+                    ...chainDataExtensions,
+                ];
+            },
+            async run() {
+                const finalBlockHeight = await getFinalBlockHeight();
+                doLog(`Final block height set to ${finalBlockHeight}`);
+                this.addGET("/backend_version", async (req, res) => {
+                    res.status(200).json(gameBackendVersion);
+                });
+                // pass endpoints to web server and run
+                (async () => startServer())();
+                runIterativeFunnel(
+                    gameStateMachine,
+                    chainFunnel,
+                    this.pollingRate,
+                    finalBlockHeight
+                );
+            },
+        };
+    },
+};
 
 async function getFinalBlockHeight(): Promise<number | null> {
-  let finalBlockHeight: number | null = null;
-  try {
-    finalBlockHeight = await fs.readFile("./stopBlockHeight.conf", "utf8").then((data) => {
-      if (!/^\d+\s*$/.test(data)) {
-        throw new Error();
-      }
-      return parseInt(data, 10);
-    });
-  } catch (err) {
-    // file doesn't exist or is invalid, finalBlockHeight remains null
-  }
-  return finalBlockHeight;
+    let finalBlockHeight: number | null = null;
+    try {
+        finalBlockHeight = await fs
+            .readFile("./stopBlockHeight.conf", "utf8")
+            .then(data => {
+                if (!/^\d+\s*$/.test(data)) {
+                    throw new Error();
+                }
+                return parseInt(data, 10);
+            });
+    } catch (err) {
+        // file doesn't exist or is invalid, finalBlockHeight remains null
+    }
+    return finalBlockHeight;
 }
 
-function stopBlockReached(latestReadBlockHeight: number, finalBlockHeight: number | null): boolean {
-  const result = finalBlockHeight !== null && latestReadBlockHeight >= finalBlockHeight;
-  run = !result;
-  return result;
+function stopBlockReached(
+    latestReadBlockHeight: number,
+    finalBlockHeight: number | null
+): boolean {
+    const result =
+        finalBlockHeight !== null && latestReadBlockHeight >= finalBlockHeight;
+    run = !result;
+    return result;
 }
 
-async function runIterativeFunnel(gameStateMachine: GameStateMachine, chainFunnel: ChainFunnel, pollingRate: number, finalBlockHeight: number | null) {
-  while (run) {
-    const latestReadBlockHeight = await gameStateMachine.latestBlockHeight();
+async function runIterativeFunnel(
+    gameStateMachine: GameStateMachine,
+    chainFunnel: ChainFunnel,
+    pollingRate: number,
+    finalBlockHeight: number | null
+) {
+    while (run) {
+        const latestReadBlockHeight =
+            await gameStateMachine.latestBlockHeight();
 
-    // Read latest chain data from funnel
-    const latestChainDataList = await chainFunnel.readData(latestReadBlockHeight + 1) as ChainData[];
-    doLog(`Received chain data from Paima Funnel. Total count: ${latestChainDataList.length}`);
-    // Checking if should safely close after fetching all chain data
-    // which may take some time
-    if (!run) {
-      process.exit(0)
-    }
-
-    // If chain data list
-    if (!latestChainDataList || !latestChainDataList?.length) {
-      console.log(`No chain data was returned, waiting ${pollingRate}s.`)
-      await delay(pollingRate * 1000);
-    }
-    else{
-      for (let block of latestChainDataList) {
-        // Checking if should safely close in between processing blocks
+        // Read latest chain data from funnel
+        const latestChainDataList = (await chainFunnel.readData(
+            latestReadBlockHeight + 1
+        )) as ChainData[];
+        doLog(
+            `Received chain data from Paima Funnel. Total count: ${latestChainDataList.length}`
+        );
+        // Checking if should safely close after fetching all chain data
+        // which may take some time
         if (!run) {
-          process.exit(0)
+            process.exit(0);
         }
-        // await logBlock(block);
-        try {
-          await gameStateMachine.process(block);
-          await logSuccess(block);
-          if (!run) {
-            process.exit(0)
-          }
 
-          const latestReadBlockHeight = await gameStateMachine.latestBlockHeight();
-          await snapshotIfTime(latestReadBlockHeight);
-          if (stopBlockReached(latestReadBlockHeight, finalBlockHeight)) {
-            process.exit(0)
-          }
+        // If chain data list
+        if (!latestChainDataList || !latestChainDataList?.length) {
+            console.log(`No chain data was returned, waiting ${pollingRate}s.`);
+            await delay(pollingRate * 1000);
+        } else {
+            for (let block of latestChainDataList) {
+                // Checking if should safely close in between processing blocks
+                if (!run) {
+                    process.exit(0);
+                }
+                // await logBlock(block);
+                try {
+                    await gameStateMachine.process(block);
+                    await logSuccess(block);
+                    if (!run) {
+                        process.exit(0);
+                    }
+
+                    const latestReadBlockHeight =
+                        await gameStateMachine.latestBlockHeight();
+                    await snapshotIfTime(latestReadBlockHeight);
+                    if (
+                        stopBlockReached(
+                            latestReadBlockHeight,
+                            finalBlockHeight
+                        )
+                    ) {
+                        process.exit(0);
+                    }
+                } catch (error) {
+                    await logError(error);
+                }
+            }
         }
-        catch (error) {
-          await logError(error)
-        }
-      }
     }
-  }
-  process.exit(0);
+    process.exit(0);
 }
 
 function delay(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms))
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-export default paimaEngine
+export default paimaEngine;
--- runtime/src/server.ts
+++ runtime/src/server.ts
@@ -1,10 +1,10 @@
-import type {Express} from "express";
-import express from "express";
 import cors from "cors";
+import type { Express } from "express";
+import express from "express";
 
 const server: Express = express();
 const bodyParser = express.json();
-const port = 3333; // default port to listen  
+const port = 3333; // default port to listen
 
 server.use(cors());
 server.use(bodyParser);
