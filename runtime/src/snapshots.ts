9dafc88fc9cfe94df0b4d4b44fb094389567052d
Martin Tomko <martin.tomko@vacuumlabs.com>
2022-10-13 14:20:03 +0200

    Made snapshot saving get pg port from env variable (if available)

--- runtime/src/snapshots.ts
+++ runtime/src/snapshots.ts
@@ -45,10 +45,11 @@ async function saveSnapshot(blockHeight: number) {
     const password = process.env.DB_PW;
     const database = process.env.DB_NAME;
     const host = process.env.DB_HOST;
+    const port = process.env.DB_PORT || "5432"
     const fileName = `paima-snapshot-${blockHeight}.sql`;
     doLog(`Attempting to save snapshot: ${fileName}`);
     exec(
-        `pg_dump --dbname=postgresql://${username}:${password}@${host}:5432/${database} -f ${snapshotPath(
+        `pg_dump --dbname=postgresql://${username}:${password}@${host}:${port}/${database} -f ${snapshotPath(
             fileName
         )}`
     );
