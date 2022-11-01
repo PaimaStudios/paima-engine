c226a6dfb3705249b2e78393942ca52e1eb4c7b5
mirtyl-wacdec <ycl2@protonmail.com>
2022-11-01 18:27:26 +0900

    further cleanup

--- paima-sm/src/paimasm/randomness.d.ts
+++
@@ -1,4 +0,0 @@
-import pg from "pg";
-export declare function randomnessRouter(n: number): typeof getSeed1;
-declare function getSeed1(latestChainData: any, DBConn: pg.Pool): Promise<string>;
-export {};
