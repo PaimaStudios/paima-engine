39924de162bfd1b8036ed2bf866b6cc87ef9c289
mirtyl-wacdec <ycl2@protonmail.com>
2022-11-07 21:49:27 +0900

    changes to sql queries on paima sm after pgtyped version bump

--- paima-sm/src/sql/queries.queries.ts
+++ paima-sm/src/sql/queries.queries.ts
@@ -17,7 +17,7 @@ export interface IGetLatestBlockHeightQuery {
   result: IGetLatestBlockHeightResult;
 }
 
-const getLatestBlockHeightIR: any = {"name":"getLatestBlockHeight","params":[],"usedParamSet":{},"statement":{"body":"SELECT * FROM block_heights \nORDER BY block_height DESC\nLIMIT 1","loc":{"a":33,"b":95,"line":2,"col":0}}};
+const getLatestBlockHeightIR: any = {"usedParamSet":{},"params":[],"statement":"SELECT * FROM block_heights \nORDER BY block_height DESC\nLIMIT 1"};
 
 /**
  * Query generated from SQL:
@@ -44,7 +44,7 @@ export interface IGetRandomnessQuery {
   result: IGetRandomnessResult;
 }
 
-const getRandomnessIR: any = {"name":"getRandomness","params":[],"usedParamSet":{},"statement":{"body":"SELECT seed FROM block_heights\nORDER BY block_height DESC\nLIMIT 25","loc":{"a":125,"b":190,"line":7,"col":0}}};
+const getRandomnessIR: any = {"usedParamSet":{},"params":[],"statement":"SELECT seed FROM block_heights\nORDER BY block_height DESC\nLIMIT 25"};
 
 /**
  * Query generated from SQL:
@@ -75,7 +75,7 @@ export interface IGetScheduledDataByBlockHeightQuery {
   result: IGetScheduledDataByBlockHeightResult;
 }
 
-const getScheduledDataByBlockHeightIR: any = {"name":"getScheduledDataByBlockHeight","params":[{"name":"block_height","required":true,"transform":{"type":"scalar"},"codeRefs":{"used":[{"a":287,"b":299,"line":13,"col":22}]}}],"usedParamSet":{"block_height":true},"statement":{"body":"SELECT * from scheduled_data\nWHERE block_height = :block_height!\nORDER BY id ASC","loc":{"a":236,"b":315,"line":12,"col":0}}};
+const getScheduledDataByBlockHeightIR: any = {"usedParamSet":{"block_height":true},"params":[{"name":"block_height","required":true,"transform":{"type":"scalar"},"locs":[{"a":50,"b":63}]}],"statement":"SELECT * from scheduled_data\nWHERE block_height = :block_height!\nORDER BY id ASC"};
 
 /**
  * Query generated from SQL:
@@ -103,7 +103,7 @@ export interface ISaveLastBlockHeightQuery {
   result: ISaveLastBlockHeightResult;
 }
 
-const saveLastBlockHeightIR: any = {"name":"saveLastBlockHeight","params":[{"name":"block_height","required":true,"transform":{"type":"scalar"},"codeRefs":{"used":[{"a":407,"b":419,"line":18,"col":9}]}},{"name":"seed","required":true,"transform":{"type":"scalar"},"codeRefs":{"used":[{"a":423,"b":427,"line":18,"col":25}]}}],"usedParamSet":{"block_height":true,"seed":true},"statement":{"body":"INSERT INTO block_heights(block_height, seed)\nVALUES (:block_height!, :seed!)","loc":{"a":352,"b":428,"line":17,"col":0}}};
+const saveLastBlockHeightIR: any = {"usedParamSet":{"block_height":true,"seed":true},"params":[{"name":"block_height","required":true,"transform":{"type":"scalar"},"locs":[{"a":54,"b":67}]},{"name":"seed","required":true,"transform":{"type":"scalar"},"locs":[{"a":70,"b":75}]}],"statement":"INSERT INTO block_heights(block_height, seed)\nVALUES (:block_height!, :seed!)"};
 
 /**
  * Query generated from SQL:
@@ -129,7 +129,7 @@ export interface IBlockHeightDoneQuery {
   result: IBlockHeightDoneResult;
 }
 
-const blockHeightDoneIR: any = {"name":"blockHeightDone","params":[{"name":"block_height","required":true,"transform":{"type":"scalar"},"codeRefs":{"used":[{"a":519,"b":531,"line":24,"col":22}]}}],"usedParamSet":{"block_height":true},"statement":{"body":"UPDATE block_heights\nSET\ndone = true\nWHERE block_height = :block_height!","loc":{"a":460,"b":531,"line":21,"col":0}}};
+const blockHeightDoneIR: any = {"usedParamSet":{"block_height":true},"params":[{"name":"block_height","required":true,"transform":{"type":"scalar"},"locs":[{"a":58,"b":71}]}],"statement":"UPDATE block_heights\nSET\ndone = true\nWHERE block_height = :block_height!"};
 
 /**
  * Query generated from SQL:
@@ -157,7 +157,7 @@ export interface IDeleteScheduledQuery {
   result: IDeleteScheduledResult;
 }
 
-const deleteScheduledIR: any = {"name":"deleteScheduled","params":[{"name":"id","required":true,"transform":{"type":"scalar"},"codeRefs":{"used":[{"a":602,"b":604,"line":28,"col":12}]}}],"usedParamSet":{"id":true},"statement":{"body":"DELETE FROM scheduled_data\nWHERE id = :id!","loc":{"a":563,"b":604,"line":27,"col":0}}};
+const deleteScheduledIR: any = {"usedParamSet":{"id":true},"params":[{"name":"id","required":true,"transform":{"type":"scalar"},"locs":[{"a":38,"b":41}]}],"statement":"DELETE FROM scheduled_data\nWHERE id = :id!"};
 
 /**
  * Query generated from SQL:
@@ -186,7 +186,7 @@ export interface IFindNonceQuery {
   result: IFindNonceResult;
 }
 
-const findNonceIR: any = {"name":"findNonce","params":[{"name":"nonce","required":false,"transform":{"type":"scalar"},"codeRefs":{"used":[{"a":666,"b":670,"line":32,"col":15}]}}],"usedParamSet":{"nonce":true},"statement":{"body":"SELECT * FROM nonces\nWHERE nonce = :nonce","loc":{"a":630,"b":670,"line":31,"col":0}}};
+const findNonceIR: any = {"usedParamSet":{"nonce":true},"params":[{"name":"nonce","required":false,"transform":{"type":"scalar"},"locs":[{"a":35,"b":40}]}],"statement":"SELECT * FROM nonces\nWHERE nonce = :nonce"};
 
 /**
  * Query generated from SQL:
@@ -213,7 +213,7 @@ export interface IInsertNonceQuery {
   result: IInsertNonceResult;
 }
 
-const insertNonceIR: any = {"name":"insertNonce","params":[{"name":"nonce","required":true,"transform":{"type":"scalar"},"codeRefs":{"used":[{"a":747,"b":752,"line":36,"col":9}]}},{"name":"block_height","required":true,"transform":{"type":"scalar"},"codeRefs":{"used":[{"a":756,"b":768,"line":36,"col":18}]}}],"usedParamSet":{"nonce":true,"block_height":true},"statement":{"body":"INSERT INTO nonces(nonce, block_height)\nVALUES (:nonce!, :block_height!)","loc":{"a":698,"b":769,"line":35,"col":0}}};
+const insertNonceIR: any = {"usedParamSet":{"nonce":true,"block_height":true},"params":[{"name":"nonce","required":true,"transform":{"type":"scalar"},"locs":[{"a":48,"b":54}]},{"name":"block_height","required":true,"transform":{"type":"scalar"},"locs":[{"a":57,"b":70}]}],"statement":"INSERT INTO nonces(nonce, block_height)\nVALUES (:nonce!, :block_height!)"};
 
 /**
  * Query generated from SQL:
