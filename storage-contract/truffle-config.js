ba2e799a1b6c027ebc7cd69d5a2e5018b6634f82
Martin Tomko <martin.tomko@vacuumlabs.com>
2022-11-16 16:32:35 +0100

    truffle-config.js autoformat

--- storage-contract/truffle-config.js
+++ storage-contract/truffle-config.js
@@ -2,32 +2,32 @@ const privateKey = process.env.PRIVATE_KEY;
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
-    contracts_directory: "./src/contract",
-    contracts_build_directory: "./build",
-    migrations_directory: "./src/migrations",
-    compilers: {
-        solc: {
-            version: "0.8.13",
-            evmVersion: "berlin",
-        },
+  contracts_directory: "./src/contract",
+  contracts_build_directory: "./build",
+  migrations_directory: "./src/migrations",
+  compilers: {
+    solc: {
+      version: "0.8.13",
+      evmVersion: "berlin",
     },
-    contract_config: {
-      owner: "0x1867Cd64DE4F9aEcfbC14846bc736cd7008dca40",
-      fee: 10n ** 16n
+  },
+  contract_config: {
+    owner: "0x1867Cd64DE4F9aEcfbC14846bc736cd7008dca40",
+    fee: 10n ** 16n
+  },
+  networks: {
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
     },
-    networks: {
-      ganache: {
-        host: "127.0.0.1",
-        port: 7545,
-        network_id: "*",
-      },
-      testnet: {
-        provider: () =>
-          new HDWalletProvider({
-            privateKeys: [privateKey],
-            providerOrUrl: "https://rpc-devnet-cardano-evm.c1.milkomeda.com",
-          }),
-        network_id: 200101,
-      },
+    testnet: {
+      provider: () =>
+        new HDWalletProvider({
+          privateKeys: [privateKey],
+          providerOrUrl: "https://rpc-devnet-cardano-evm.c1.milkomeda.com",
+        }),
+      network_id: 200101,
     },
+  },
 };
