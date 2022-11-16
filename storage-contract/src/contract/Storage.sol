019c975e86c3730cab920d15dc7a783a7a13b4fe
Martin Tomko <martin.tomko@vacuumlabs.com>
2022-11-16 15:59:41 +0100

    Autoformat of contract code + its tests

--- storage-contract/src/contract/Storage.sol
+++ storage-contract/src/contract/Storage.sol
@@ -2,93 +2,93 @@
 pragma solidity ^0.8.13;
 
 contract Storage {
-    event PaimaGameInteraction(address indexed userAddress, bytes data, uint256 value);
+  event PaimaGameInteraction(address indexed userAddress, bytes data, uint256 value);
 
-    address public owner;
-    uint256 public fee;
-    mapping(address => uint256) public latestStoreHeight;
+  address public owner;
+  uint256 public fee;
+  mapping(address => uint256) public latestStoreHeight;
 
-    constructor(address _owner, uint256 _fee) {
-        owner = _owner;
-        fee = _fee;
-    }
+  constructor(address _owner, uint256 _fee) {
+    owner = _owner;
+    fee = _fee;
+  }
 
-    function paimaSubmitGameInput(bytes memory data) public payable {
-        require(msg.value >= fee, "Sufficient funds required to submit game input");
-        latestStoreHeight[msg.sender] = block.number;
-        emit PaimaGameInteraction(msg.sender, data, msg.value);
-    }
+  function paimaSubmitGameInput(bytes memory data) public payable {
+    require(msg.value >= fee, 'Sufficient funds required to submit game input');
+    latestStoreHeight[msg.sender] = block.number;
+    emit PaimaGameInteraction(msg.sender, data, msg.value);
+  }
 
-    function createLobby(bytes memory data) public payable {
-        paimaSubmitGameInput(data);
-    }
+  function createLobby(bytes memory data) public payable {
+    paimaSubmitGameInput(data);
+  }
 
-    function joinLobby(bytes memory data) public payable {
-        paimaSubmitGameInput(data);
-    }
+  function joinLobby(bytes memory data) public payable {
+    paimaSubmitGameInput(data);
+  }
 
-    function submitMoves(bytes memory data) public payable {
-        paimaSubmitGameInput(data);
-    }
+  function submitMoves(bytes memory data) public payable {
+    paimaSubmitGameInput(data);
+  }
 
-    function setNft(bytes memory data) public payable {
-        paimaSubmitGameInput(data);
-    }
+  function setNft(bytes memory data) public payable {
+    paimaSubmitGameInput(data);
+  }
 
-    function submitAuxiliaryGameInput1(bytes memory data) public payable {
-        paimaSubmitGameInput(data);
-    }
+  function submitAuxiliaryGameInput1(bytes memory data) public payable {
+    paimaSubmitGameInput(data);
+  }
 
-    function submitAuxiliaryGameInput2(bytes memory data) public payable {
-        paimaSubmitGameInput(data);
-    }
+  function submitAuxiliaryGameInput2(bytes memory data) public payable {
+    paimaSubmitGameInput(data);
+  }
 
-    function submitAuxiliaryGameInput3(bytes memory data) public payable {
-        paimaSubmitGameInput(data);
-    }
+  function submitAuxiliaryGameInput3(bytes memory data) public payable {
+    paimaSubmitGameInput(data);
+  }
 
-    function submitAuxiliaryGameInput4(bytes memory data) public payable {
-        paimaSubmitGameInput(data);
-    }
+  function submitAuxiliaryGameInput4(bytes memory data) public payable {
+    paimaSubmitGameInput(data);
+  }
 
-    function submitAuxiliaryGameInput5(bytes memory data) public payable {
-        paimaSubmitGameInput(data);
-    }
+  function submitAuxiliaryGameInput5(bytes memory data) public payable {
+    paimaSubmitGameInput(data);
+  }
 
-    function submitAuxiliaryGameInput6(bytes memory data) public payable {
-        paimaSubmitGameInput(data);
-    }
+  function submitAuxiliaryGameInput6(bytes memory data) public payable {
+    paimaSubmitGameInput(data);
+  }
 
-    function submitAuxiliaryGameInput7(bytes memory data) public payable {
-        paimaSubmitGameInput(data);
-    }
+  function submitAuxiliaryGameInput7(bytes memory data) public payable {
+    paimaSubmitGameInput(data);
+  }
 
-    function submitAuxiliaryGameInput8(bytes memory data) public payable {
-        paimaSubmitGameInput(data);
-    }
+  function submitAuxiliaryGameInput8(bytes memory data) public payable {
+    paimaSubmitGameInput(data);
+  }
 
-    function submitAuxiliaryGameInput9(bytes memory data) public payable {
-        paimaSubmitGameInput(data);
-    }
+  function submitAuxiliaryGameInput9(bytes memory data) public payable {
+    paimaSubmitGameInput(data);
+  }
 
-    function submitAuxiliaryGameInput10(bytes memory data) public payable {
-        paimaSubmitGameInput(data);
-    }
+  function submitAuxiliaryGameInput10(bytes memory data) public payable {
+    paimaSubmitGameInput(data);
+  }
 
-    function withdrawFunds() public {
-        require(msg.sender == owner, "Only owner can withdraw funds");
-        address payable to = payable(owner);
-        uint256 balance = address(this).balance;
-        to.transfer(balance);
-    }
+  function withdrawFunds() public {
+    require(msg.sender == owner, 'Only owner can withdraw funds');
+    address payable to = payable(owner);
+    uint256 balance = address(this).balance;
+    to.transfer(balance);
+  }
 
-    function setOwner(address newOwner) public {
-        require(msg.sender == owner, "Only owner can change owner");
-        owner = newOwner;
-    }
+  function setOwner(address newOwner) public {
+    require(msg.sender == owner, 'Only owner can change owner');
+    owner = newOwner;
+  }
 
-    function setFee(uint256 newFee) public {
-        require(msg.sender == owner, "Only owner can change fee");
-        fee = newFee;
-    }
+  function setFee(uint256 newFee) public {
+    require(msg.sender == owner, 'Only owner can change fee');
+    fee = newFee;
+  }
 }
--- storage-contract/test/StorageTest.t.sol
+++ storage-contract/test/StorageTest.t.sol
@@ -1,237 +1,237 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../lib/cheatcodes.sol";
-import "../lib/console.sol";
-import "../lib/ctest.sol";
+import '../lib/cheatcodes.sol';
+import '../lib/console.sol';
+import '../lib/ctest.sol';
 
-import "../src/contract/Storage.sol";
+import '../src/contract/Storage.sol';
 
 contract StorageTest is CTest {
-    event PaimaGameInteraction(address indexed userAddress, bytes data, uint256 value);
+  event PaimaGameInteraction(address indexed userAddress, bytes data, uint256 value);
 
-    CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
-    Storage strg;
+  CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
+  Storage strg;
 
-    address account1 = 0x766FCe3d50d795Fe6DcB1020AB58bccddd5C5c77;
-    address account2 = 0x078D888E40faAe0f32594342c85940AF3949E666;
+  address account1 = 0x766FCe3d50d795Fe6DcB1020AB58bccddd5C5c77;
+  address account2 = 0x078D888E40faAe0f32594342c85940AF3949E666;
 
-    uint256 constant MAX_INT = 2**256 - 1;
-    uint256 constant FEE = 1e16;
+  uint256 constant MAX_INT = 2**256 - 1;
+  uint256 constant FEE = 1e16;
 
-    function setUp() public {
-        strg = new Storage(address(account1), FEE);
-        cheats.deal(account1, 100 ether);
-        cheats.deal(account2, 100 ether);
-    }
+  function setUp() public {
+    strg = new Storage(address(account1), FEE);
+    cheats.deal(account1, 100 ether);
+    cheats.deal(account2, 100 ether);
+  }
 
-    function testWithdraw() public {
-        cheats.prank(account1);
-        strg.withdrawFunds();
-        assertEq(address(strg).balance, 0);
-    }
+  function testWithdraw() public {
+    cheats.prank(account1);
+    strg.withdrawFunds();
+    assertEq(address(strg).balance, 0);
+  }
 
-    function testCannotWithdraw() public {
-        cheats.prank(account2);
-        cheats.expectRevert("Only owner can withdraw funds");
-        strg.withdrawFunds();
-    }
+  function testCannotWithdraw() public {
+    cheats.prank(account2);
+    cheats.expectRevert('Only owner can withdraw funds');
+    strg.withdrawFunds();
+  }
 
-    function testCannotStoreWithoutFee() public {
-        cheats.startPrank(account2);
-        cheats.expectRevert("Sufficient funds required to submit game input");
-        strg.paimaSubmitGameInput("0x123456");
-        cheats.expectRevert("Sufficient funds required to submit game input");
-        strg.paimaSubmitGameInput{value: FEE - 1}("0x123456");
-        cheats.stopPrank();
-    }
+  function testCannotStoreWithoutFee() public {
+    cheats.startPrank(account2);
+    cheats.expectRevert('Sufficient funds required to submit game input');
+    strg.paimaSubmitGameInput('0x123456');
+    cheats.expectRevert('Sufficient funds required to submit game input');
+    strg.paimaSubmitGameInput{value: FEE - 1}('0x123456');
+    cheats.stopPrank();
+  }
 
-    function testStore() public {
-        cheats.prank(account2);
-        bytes memory data = "0x123456";
-        cheats.expectEmit(true, true, true, true);
-        emit PaimaGameInteraction(account2, data, FEE);
-        strg.paimaSubmitGameInput{value: FEE}(data);
-        assertEq(address(strg).balance, FEE);
-    }
+  function testStore() public {
+    cheats.prank(account2);
+    bytes memory data = '0x123456';
+    cheats.expectEmit(true, true, true, true);
+    emit PaimaGameInteraction(account2, data, FEE);
+    strg.paimaSubmitGameInput{value: FEE}(data);
+    assertEq(address(strg).balance, FEE);
+  }
 
-    function testSpecificStores() public {
-        cheats.startPrank(account2);
-        bytes memory data = "0x123456";
+  function testSpecificStores() public {
+    cheats.startPrank(account2);
+    bytes memory data = '0x123456';
 
-        cheats.expectEmit(true, true, true, true);
-        emit PaimaGameInteraction(account2, data, FEE);
-        strg.createLobby{value: FEE}(data);
+    cheats.expectEmit(true, true, true, true);
+    emit PaimaGameInteraction(account2, data, FEE);
+    strg.createLobby{value: FEE}(data);
 
-        cheats.expectEmit(true, true, true, true);
-        emit PaimaGameInteraction(account2, data, FEE);
-        strg.joinLobby{value: FEE}(data);
+    cheats.expectEmit(true, true, true, true);
+    emit PaimaGameInteraction(account2, data, FEE);
+    strg.joinLobby{value: FEE}(data);
 
-        cheats.expectEmit(true, true, true, true);
-        emit PaimaGameInteraction(account2, data, FEE);
-        strg.submitMoves{value: FEE}(data);
+    cheats.expectEmit(true, true, true, true);
+    emit PaimaGameInteraction(account2, data, FEE);
+    strg.submitMoves{value: FEE}(data);
 
-        cheats.expectEmit(true, true, true, true);
-        emit PaimaGameInteraction(account2, data, FEE);
-        strg.setNft{value: FEE}(data);
+    cheats.expectEmit(true, true, true, true);
+    emit PaimaGameInteraction(account2, data, FEE);
+    strg.setNft{value: FEE}(data);
 
-        cheats.expectEmit(true, true, true, true);
-        emit PaimaGameInteraction(account2, data, FEE);
-        strg.submitAuxiliaryGameInput1{value: FEE}(data);
+    cheats.expectEmit(true, true, true, true);
+    emit PaimaGameInteraction(account2, data, FEE);
+    strg.submitAuxiliaryGameInput1{value: FEE}(data);
 
-        cheats.expectEmit(true, true, true, true);
-        emit PaimaGameInteraction(account2, data, FEE);
-        strg.submitAuxiliaryGameInput2{value: FEE}(data);
+    cheats.expectEmit(true, true, true, true);
+    emit PaimaGameInteraction(account2, data, FEE);
+    strg.submitAuxiliaryGameInput2{value: FEE}(data);
 
-        cheats.expectEmit(true, true, true, true);
-        emit PaimaGameInteraction(account2, data, FEE);
-        strg.submitAuxiliaryGameInput3{value: FEE}(data);
+    cheats.expectEmit(true, true, true, true);
+    emit PaimaGameInteraction(account2, data, FEE);
+    strg.submitAuxiliaryGameInput3{value: FEE}(data);
 
-        cheats.expectEmit(true, true, true, true);
-        emit PaimaGameInteraction(account2, data, FEE);
-        strg.submitAuxiliaryGameInput4{value: FEE}(data);
+    cheats.expectEmit(true, true, true, true);
+    emit PaimaGameInteraction(account2, data, FEE);
+    strg.submitAuxiliaryGameInput4{value: FEE}(data);
 
-        cheats.expectEmit(true, true, true, true);
-        emit PaimaGameInteraction(account2, data, FEE);
-        strg.submitAuxiliaryGameInput5{value: FEE}(data);
+    cheats.expectEmit(true, true, true, true);
+    emit PaimaGameInteraction(account2, data, FEE);
+    strg.submitAuxiliaryGameInput5{value: FEE}(data);
 
-        cheats.expectEmit(true, true, true, true);
-        emit PaimaGameInteraction(account2, data, FEE);
-        strg.submitAuxiliaryGameInput6{value: FEE}(data);
+    cheats.expectEmit(true, true, true, true);
+    emit PaimaGameInteraction(account2, data, FEE);
+    strg.submitAuxiliaryGameInput6{value: FEE}(data);
 
-        cheats.expectEmit(true, true, true, true);
-        emit PaimaGameInteraction(account2, data, FEE);
-        strg.submitAuxiliaryGameInput7{value: FEE}(data);
+    cheats.expectEmit(true, true, true, true);
+    emit PaimaGameInteraction(account2, data, FEE);
+    strg.submitAuxiliaryGameInput7{value: FEE}(data);
 
-        cheats.expectEmit(true, true, true, true);
-        emit PaimaGameInteraction(account2, data, FEE);
-        strg.submitAuxiliaryGameInput8{value: FEE}(data);
+    cheats.expectEmit(true, true, true, true);
+    emit PaimaGameInteraction(account2, data, FEE);
+    strg.submitAuxiliaryGameInput8{value: FEE}(data);
 
-        cheats.expectEmit(true, true, true, true);
-        emit PaimaGameInteraction(account2, data, FEE);
-        strg.submitAuxiliaryGameInput9{value: FEE}(data);
+    cheats.expectEmit(true, true, true, true);
+    emit PaimaGameInteraction(account2, data, FEE);
+    strg.submitAuxiliaryGameInput9{value: FEE}(data);
 
-        cheats.expectEmit(true, true, true, true);
-        emit PaimaGameInteraction(account2, data, FEE);
-        strg.submitAuxiliaryGameInput10{value: FEE}(data);
+    cheats.expectEmit(true, true, true, true);
+    emit PaimaGameInteraction(account2, data, FEE);
+    strg.submitAuxiliaryGameInput10{value: FEE}(data);
 
-        cheats.stopPrank();
-        assertEq(address(strg).balance, 14 * FEE);
-    }
+    cheats.stopPrank();
+    assertEq(address(strg).balance, 14 * FEE);
+  }
 
-    function testCannotSpecificStoresWithoutFee() public {
-        cheats.startPrank(account2);
-        bytes memory data = "0x123456";
+  function testCannotSpecificStoresWithoutFee() public {
+    cheats.startPrank(account2);
+    bytes memory data = '0x123456';
 
-        cheats.expectRevert("Sufficient funds required to submit game input");
-        strg.createLobby{value: FEE - 1}(data);
+    cheats.expectRevert('Sufficient funds required to submit game input');
+    strg.createLobby{value: FEE - 1}(data);
 
-        cheats.expectRevert("Sufficient funds required to submit game input");
-        strg.joinLobby{value: FEE - 1}(data);
+    cheats.expectRevert('Sufficient funds required to submit game input');
+    strg.joinLobby{value: FEE - 1}(data);
 
-        cheats.expectRevert("Sufficient funds required to submit game input");
-        strg.submitMoves{value: FEE - 1}(data);
+    cheats.expectRevert('Sufficient funds required to submit game input');
+    strg.submitMoves{value: FEE - 1}(data);
 
-        cheats.expectRevert("Sufficient funds required to submit game input");
-        strg.setNft{value: FEE - 1}(data);
+    cheats.expectRevert('Sufficient funds required to submit game input');
+    strg.setNft{value: FEE - 1}(data);
 
-        cheats.expectRevert("Sufficient funds required to submit game input");
-        strg.submitAuxiliaryGameInput1{value: FEE - 1}(data);
+    cheats.expectRevert('Sufficient funds required to submit game input');
+    strg.submitAuxiliaryGameInput1{value: FEE - 1}(data);
 
-        cheats.expectRevert("Sufficient funds required to submit game input");
-        strg.submitAuxiliaryGameInput2{value: FEE - 1}(data);
+    cheats.expectRevert('Sufficient funds required to submit game input');
+    strg.submitAuxiliaryGameInput2{value: FEE - 1}(data);
 
-        cheats.expectRevert("Sufficient funds required to submit game input");
-        strg.submitAuxiliaryGameInput3{value: FEE - 1}(data);
+    cheats.expectRevert('Sufficient funds required to submit game input');
+    strg.submitAuxiliaryGameInput3{value: FEE - 1}(data);
 
-        cheats.expectRevert("Sufficient funds required to submit game input");
-        strg.submitAuxiliaryGameInput4{value: FEE - 1}(data);
+    cheats.expectRevert('Sufficient funds required to submit game input');
+    strg.submitAuxiliaryGameInput4{value: FEE - 1}(data);
 
-        cheats.expectRevert("Sufficient funds required to submit game input");
-        strg.submitAuxiliaryGameInput5{value: FEE - 1}(data);
+    cheats.expectRevert('Sufficient funds required to submit game input');
+    strg.submitAuxiliaryGameInput5{value: FEE - 1}(data);
 
-        cheats.expectRevert("Sufficient funds required to submit game input");
-        strg.submitAuxiliaryGameInput6{value: FEE - 1}(data);
+    cheats.expectRevert('Sufficient funds required to submit game input');
+    strg.submitAuxiliaryGameInput6{value: FEE - 1}(data);
 
-        cheats.expectRevert("Sufficient funds required to submit game input");
-        strg.submitAuxiliaryGameInput7{value: FEE - 1}(data);
+    cheats.expectRevert('Sufficient funds required to submit game input');
+    strg.submitAuxiliaryGameInput7{value: FEE - 1}(data);
 
-        cheats.expectRevert("Sufficient funds required to submit game input");
-        strg.submitAuxiliaryGameInput8{value: FEE - 1}(data);
+    cheats.expectRevert('Sufficient funds required to submit game input');
+    strg.submitAuxiliaryGameInput8{value: FEE - 1}(data);
 
-        cheats.expectRevert("Sufficient funds required to submit game input");
-        strg.submitAuxiliaryGameInput9{value: FEE - 1}(data);
+    cheats.expectRevert('Sufficient funds required to submit game input');
+    strg.submitAuxiliaryGameInput9{value: FEE - 1}(data);
 
-        cheats.expectRevert("Sufficient funds required to submit game input");
-        strg.submitAuxiliaryGameInput10{value: FEE - 1}(data);
+    cheats.expectRevert('Sufficient funds required to submit game input');
+    strg.submitAuxiliaryGameInput10{value: FEE - 1}(data);
 
-        cheats.stopPrank();
-    }
+    cheats.stopPrank();
+  }
 
-    function testStoreAndWithdraw() public {
-        cheats.startPrank(account2);
-        bytes memory data = "0x123456";
-        strg.paimaSubmitGameInput{value: FEE}(data);
-        strg.paimaSubmitGameInput{value: FEE}(data);
-        strg.paimaSubmitGameInput{value: FEE}(data);
-        cheats.stopPrank();
-        assertEq(address(strg).balance, FEE * 3);
-        cheats.prank(account1);
-        strg.withdrawFunds();
-        assertEq(address(strg).balance, 0);
-    }
+  function testStoreAndWithdraw() public {
+    cheats.startPrank(account2);
+    bytes memory data = '0x123456';
+    strg.paimaSubmitGameInput{value: FEE}(data);
+    strg.paimaSubmitGameInput{value: FEE}(data);
+    strg.paimaSubmitGameInput{value: FEE}(data);
+    cheats.stopPrank();
+    assertEq(address(strg).balance, FEE * 3);
+    cheats.prank(account1);
+    strg.withdrawFunds();
+    assertEq(address(strg).balance, 0);
+  }
 
-    function testBlockHeight() public {
-        assertEq(strg.latestStoreHeight(account2), 0);
-        uint256 newHeight = 101;
-        cheats.roll(newHeight);
-        cheats.prank(account2);
-        strg.paimaSubmitGameInput{value: FEE}("0x123456");
-        assertEq(strg.latestStoreHeight(account2), newHeight);
-    }
+  function testBlockHeight() public {
+    assertEq(strg.latestStoreHeight(account2), 0);
+    uint256 newHeight = 101;
+    cheats.roll(newHeight);
+    cheats.prank(account2);
+    strg.paimaSubmitGameInput{value: FEE}('0x123456');
+    assertEq(strg.latestStoreHeight(account2), newHeight);
+  }
 
-    function testChangeOwner() public {
-        assertEq(strg.owner(), account1);
-        cheats.prank(account1);
-        strg.setOwner(account2);
-        assertEq(strg.owner(), account2);
-        cheats.prank(account2);
-        strg.withdrawFunds();
-    }
+  function testChangeOwner() public {
+    assertEq(strg.owner(), account1);
+    cheats.prank(account1);
+    strg.setOwner(account2);
+    assertEq(strg.owner(), account2);
+    cheats.prank(account2);
+    strg.withdrawFunds();
+  }
 
-    function testCannotChangeOwner() public {
-        assertEq(strg.owner(), account1);
-        cheats.prank(account2);
-        cheats.expectRevert("Only owner can change owner");
-        strg.setOwner(account1);
-        assertEq(strg.owner(), account1);
-    }
+  function testCannotChangeOwner() public {
+    assertEq(strg.owner(), account1);
+    cheats.prank(account2);
+    cheats.expectRevert('Only owner can change owner');
+    strg.setOwner(account1);
+    assertEq(strg.owner(), account1);
+  }
 
-    function testChangeFee() public {
-        uint256 newFee = FEE * 2;
-        assertEq(strg.owner(), account1);
-        assertEq(strg.fee(), FEE);
-        cheats.prank(account1);
-        strg.setFee(newFee);
-        assertEq(strg.fee(), newFee);
+  function testChangeFee() public {
+    uint256 newFee = FEE * 2;
+    assertEq(strg.owner(), account1);
+    assertEq(strg.fee(), FEE);
+    cheats.prank(account1);
+    strg.setFee(newFee);
+    assertEq(strg.fee(), newFee);
 
-        bytes memory data = "0x123456";
-        cheats.prank(account2);
-        cheats.expectRevert("Sufficient funds required to submit game input");
-        strg.paimaSubmitGameInput{value: newFee - 1}(data);
+    bytes memory data = '0x123456';
+    cheats.prank(account2);
+    cheats.expectRevert('Sufficient funds required to submit game input');
+    strg.paimaSubmitGameInput{value: newFee - 1}(data);
 
-        cheats.prank(account2);
-        cheats.expectEmit(true, true, true, true);
-        emit PaimaGameInteraction(account2, data, newFee);
-        strg.paimaSubmitGameInput{value: newFee}(data);
-        assertEq(address(strg).balance, newFee);
-    }
+    cheats.prank(account2);
+    cheats.expectEmit(true, true, true, true);
+    emit PaimaGameInteraction(account2, data, newFee);
+    strg.paimaSubmitGameInput{value: newFee}(data);
+    assertEq(address(strg).balance, newFee);
+  }
 
-    function testCannotChangeFee() public {
-        assertEq(strg.fee(), FEE);
-        cheats.prank(account2);
-        cheats.expectRevert("Only owner can change fee");
-        strg.setFee(FEE * 2);
-        assertEq(strg.fee(), FEE);
-    }
+  function testCannotChangeFee() public {
+    assertEq(strg.fee(), FEE);
+    cheats.prank(account2);
+    cheats.expectRevert('Only owner can change fee');
+    strg.setFee(FEE * 2);
+    assertEq(strg.fee(), FEE);
+  }
 }
