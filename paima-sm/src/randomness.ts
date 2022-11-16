10379af6fd8b2c9f21144e9f7bb8b187d3466bd5
Lukas Miskovsky <miskovskylukas@gmail.com>
2022-11-16 13:01:39 +0100

    Add initial version of improved randomness

--- paima-sm/src/randomness.ts
+++ paima-sm/src/randomness.ts
@@ -1,3 +1,4 @@
+import Prando from '@paima/prando';
 import type { ChainData } from '@paima/utils';
 import Crypto from 'crypto';
 import type pg from 'pg';
@@ -9,10 +10,38 @@ export function randomnessRouter(n: number): typeof getSeed1 {
   else throw Error('wrong randomness protocol set');
 }
 
-// Basic randomness generation protocol which hashes together previous seeds + latest block hash
+function chooseData(chainData: ChainData, seed: string): string[] {
+  const submittedData = chainData.submittedData
+    .map(data => [
+      //TODO: use consumer to decode the inputData
+      data.inputData,
+      data.inputNonce,
+      data.userAddress,
+    ])
+    .flat();
+  const relevantData = [
+    chainData.timestamp.toString(),
+    chainData.blockNumber.toString(),
+    chainData.blockHash,
+    ...submittedData,
+  ];
+  const prando = new Prando(seed);
+  const randomSelection = (): boolean => {
+    const randomValue = Math.round(prando.next());
+    return randomValue === 1 ? true : false;
+  };
+
+  return relevantData.filter(randomSelection);
+}
+
+/*
+ * Basic randomness generation protocol which hashes together previous seeds and randomly selected chain data
+ */
 async function getSeed1(latestChainData: ChainData, DBConn: pg.Pool): Promise<string> {
-  const hashes = await getRandomness.run(undefined, DBConn);
-  const seed = hashTogether([latestChainData.blockHash, ...hashes.map(h => h.seed)]);
+  const seeds = (await getRandomness.run(undefined, DBConn)).map(result => result.seed);
+  const interimSeed = hashTogether([latestChainData.blockHash, ...seeds]);
+  const selectedChainData = chooseData(latestChainData, interimSeed);
+  const seed = hashTogether([...selectedChainData, ...seeds]);
   return seed;
 }
 
