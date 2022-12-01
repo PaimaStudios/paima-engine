2124e895595e032d3d9029b15d716e035d42b786
Martin Tomko <martin.tomko@vacuumlabs.com>
2022-12-01 18:36:30 +0100

    Fixed block_heights table queries to always consider the done field

--- paima-sm/src/sql/queries.sql
+++ paima-sm/src/sql/queries.sql
@@ -1,21 +1,18 @@
 /* @name getLatestBlockHeight */
-SELECT * FROM block_heights 
+SELECT * FROM block_heights
+WHERE done IS TRUE
 ORDER BY block_height DESC
 LIMIT 1;
 
 /* @name getBlockSeeds */
 SELECT seed FROM block_heights
+WHERE done IS TRUE
 ORDER BY block_height DESC
 LIMIT 25;
 
-/* @name getScheduledDataByBlockHeight */
-SELECT * from scheduled_data
-WHERE block_height = :block_height!
-ORDER BY id ASC;
-
 /*  @name saveLastBlockHeight */
-INSERT INTO block_heights(block_height, seed)
-VALUES (:block_height!, :seed!);
+INSERT INTO block_heights(block_height, seed, done)
+VALUES (:block_height!, :seed!, FALSE);
 
 /* @name blockHeightDone */
 UPDATE block_heights
@@ -27,9 +24,18 @@ WHERE block_height = :block_height!;
 DELETE FROM scheduled_data
 WHERE id = :id!;
 
+/* @name getScheduledDataByBlockHeight */
+SELECT * from scheduled_data
+WHERE block_height = :block_height!
+ORDER BY id ASC;
+
 /* @name findNonce */
 SELECT * FROM nonces
 WHERE nonce = :nonce;
+
+/* @name deleteNonces */
+DELETE FROM nonces
+WHERE block_height <= :limit_block_height!;
 
 /* @name insertNonce */
 INSERT INTO nonces(nonce, block_height)
