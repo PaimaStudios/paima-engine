5e083473b39cb5a40284468df96c7678db098131
Martin Tomko <martin.tomko@vacuumlabs.com>
2022-12-01 22:15:48 +0100

    Updated SM error logging

--- paima-sm/src/index.ts
+++ paima-sm/src/index.ts
@@ -1,6 +1,6 @@
 import pg from 'pg';
 
-import { doLog, SCHEDULED_DATA_ADDRESS } from '@paima/utils';
+import { doLog, logError, SCHEDULED_DATA_ADDRESS } from '@paima/utils';
 import type { SubmittedChainData } from '@paima/utils';
 import type { ChainData, GameStateMachineInitializer } from '@paima/utils';
 import Prando from '@paima/prando';
@@ -28,21 +28,21 @@ const SM: GameStateMachineInitializer = {
     const ensureReadOnly = `SET SESSION CHARACTERISTICS AS TRANSACTION READ ONLY;`;
     const readonlyset = readonlyDBConn.query(ensureReadOnly); // note: this query modifies the DB state
 
-    DBConn.on('error', err => console.log(err, 'postgres pool error'));
+    DBConn.on('error', err => logError(err));
 
     DBConn.on('connect', (_client: pg.PoolClient) => {
       // On each new client initiated, need to register for error(this is a serious bug on pg, the client throw errors although it should not)
       _client.on('error', (err: Error) => {
-        console.log(err);
+        logError(err);
       });
     });
 
-    readonlyDBConn.on('error', err => console.log(err, 'postgres pool error'));
+    readonlyDBConn.on('error', err => logError(err));
 
     readonlyDBConn.on('connect', (_client: pg.PoolClient) => {
       // On each new client initiated, need to register for error(this is a serious bug on pg, the client throw errors although it should not)
       _client.on('error', (err: Error) => {
-        console.log(err);
+        logError(err);
       });
     });
 
