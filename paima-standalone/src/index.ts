160370841c1f9186f45e41d0b2b24183c98b4571
Lukas Miskovsky <miskovskylukas@gmail.com>
2023-03-22 11:48:17 +0100

    Remove redundant healthcheck fix

--- paima-standalone/src/index.ts
+++ paima-standalone/src/index.ts
@@ -1,13 +1,9 @@
 // Load environment variables before everything else.
 import { config } from 'dotenv';
-import { ENV } from '@paima/utils';
 import { argumentRouter } from './utils/input.js';
 
 // Load environment variables
 config({ path: `${process.cwd()}/.env.${process.env.NODE_ENV || 'development'}` });
-
-// Check if .env loaded correctly
-ENV.doHealthCheck();
 
 async function main(): Promise<void> {
   await argumentRouter();
