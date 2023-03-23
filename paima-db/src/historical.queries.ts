8a947938c2e6df7284f0fbc2d450e7d6e1e8dae0
Martin Tomko <martin.tomko@vacuumlabs.com>
2023-03-23 13:16:42 +0100

    Added query to store game inputs

---
+++ paima-db/src/historical.queries.ts
@@ -0,0 +1,31 @@
+/** Types generated for queries found in "src/historical.sql" */
+import { PreparedQuery } from '@pgtyped/query';
+
+/** 'StoreGameInput' parameters type */
+export interface IStoreGameInputParams {
+  block_height: number;
+  input_data: string;
+  user_address: string;
+}
+
+/** 'StoreGameInput' return type */
+export type IStoreGameInputResult = void;
+
+/** 'StoreGameInput' query type */
+export interface IStoreGameInputQuery {
+  params: IStoreGameInputParams;
+  result: IStoreGameInputResult;
+}
+
+const storeGameInputIR: any = {"usedParamSet":{"block_height":true,"user_address":true,"input_data":true},"params":[{"name":"block_height","required":true,"transform":{"type":"scalar"},"locs":[{"a":83,"b":96}]},{"name":"user_address","required":true,"transform":{"type":"scalar"},"locs":[{"a":99,"b":112}]},{"name":"input_data","required":true,"transform":{"type":"scalar"},"locs":[{"a":115,"b":126}]}],"statement":"INSERT INTO historical_game_inputs(block_height, user_address, input_data)\nVALUES (:block_height!, :user_address!, :input_data!)"};
+
+/**
+ * Query generated from SQL:
+ * ```
+ * INSERT INTO historical_game_inputs(block_height, user_address, input_data)
+ * VALUES (:block_height!, :user_address!, :input_data!)
+ * ```
+ */
+export const storeGameInput = new PreparedQuery<IStoreGameInputParams,IStoreGameInputResult>(storeGameInputIR);
+
+
--- paima-db/src/index.ts
+++ paima-db/src/index.ts
@@ -2,7 +2,15 @@ import { tx } from './pg-tx';
 import { getConnection } from './pg-connection.js';
 import { createScheduledData, deleteScheduledData } from './scheduled-constructors';
 import { initializePaimaTables } from './database-validation';
+import { storeGameInput } from './historical.queries';
 
 export * from './types';
 
-export { tx, getConnection, createScheduledData, deleteScheduledData, initializePaimaTables };
+export {
+  tx,
+  getConnection,
+  createScheduledData,
+  deleteScheduledData,
+  initializePaimaTables,
+  storeGameInput,
+};
