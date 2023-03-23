91ff645a6445a3293f3172c664946c68971797ed
Martin Tomko <martin.tomko@vacuumlabs.com>
2023-03-23 15:58:09 +0100

    Updated exports from query code to simply export everything

--- paima-db/src/index.ts
+++ paima-db/src/index.ts
@@ -3,34 +3,11 @@ import { getConnection } from './pg-connection.js';
 import { createScheduledData, deleteScheduledData } from './scheduled-constructors';
 import { initializePaimaTables } from './database-validation';
 
-import {
-  getLatestProcessedBlockHeight,
-  getBlockHeight,
-  getBlockSeeds,
-  saveLastBlockHeight,
-  blockHeightDone,
-} from './sql/block-heights.queries';
-import { deleteScheduled, getScheduledDataByBlockHeight } from './sql/scheduled.queries';
-import { findNonce, insertNonce, deleteNonces } from './sql/nonces.queries';
-import { storeGameInput } from './sql/historical.queries';
-
 export * from './types';
 
-export {
-  tx,
-  getConnection,
-  createScheduledData,
-  deleteScheduledData,
-  initializePaimaTables,
-  getLatestProcessedBlockHeight,
-  getBlockHeight,
-  getBlockSeeds,
-  saveLastBlockHeight,
-  blockHeightDone,
-  deleteScheduled,
-  getScheduledDataByBlockHeight,
-  findNonce,
-  insertNonce,
-  deleteNonces,
-  storeGameInput,
-};
+export * from './sql/block-heights.queries';
+export * from './sql/scheduled.queries';
+export * from './sql/nonces.queries';
+export * from './sql/historical.queries';
+
+export { tx, getConnection, createScheduledData, deleteScheduledData, initializePaimaTables };
