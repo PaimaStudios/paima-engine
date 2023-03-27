fbe9e0b7eea265b1bee938dfdcf775b9966b2027
Edward A <ac.edward@gmail.com>
2023-03-27 16:31:50 -0300

    added binaries for linux/osx builds

---
+++ paima-standalone/scripts/unpack.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# uncompress linux/macos 18.15.0 debug node builds
+FILE=$HOME/.pkg-cache/v3.4/built-v18.15.0-linux-x64
+if test -f "$FILE"; then
+    echo "binary $FILE exists, skipping instalation."
+else
+
+  curl -LJO https://raw.githubusercontent.com/PaimaStudios/pkg-dev-prebuilts/main/dev-pkg-builtaa
+  curl -LJO https://raw.githubusercontent.com/PaimaStudios/pkg-dev-prebuilts/main/dev-pkg-builtab
+  curl -LJO https://raw.githubusercontent.com/PaimaStudios/pkg-dev-prebuilts/main/dev-pkg-builtac
+  curl -LJO https://raw.githubusercontent.com/PaimaStudios/pkg-dev-prebuilts/main/dev-pkg-builtad
+  curl -LJO https://raw.githubusercontent.com/PaimaStudios/pkg-dev-prebuilts/main/dev-pkg-builtae
+  curl -LJO https://raw.githubusercontent.com/PaimaStudios/pkg-dev-prebuilts/main/dev-pkg-builtaf
+
+  cat dev-pkg-built* > debug-builds.tar.gz
+
+  tar -zxvf debug-builds.tar.gz
+
+  mkdir -p $HOME/.pkg-cache/v3.4/
+
+  rm debug-builds.tar.gz dev-pkg-builta*
+
+  mv built-v18.15.0-* $HOME/.pkg-cache/v3.4/
+fi
