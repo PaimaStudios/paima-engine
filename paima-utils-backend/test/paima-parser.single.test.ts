284db8e0b8eb61f1ba22427ab937dc495da9acd1
Edward A <ac.edward@gmail.com>
2023-03-28 15:24:53 -0300

    Parser for commands 'j|'

---
+++ paima-utils-backend/test/paima-parser.single.test.ts
@@ -0,0 +1,32 @@
+import { describe, expect, test } from '@jest/globals';
+import { PaimaParser } from '../src/parser/PaimaParser';
+
+const myGrammar = `
+  join = j|
+`;
+
+const parserCommands = {
+  join: {}
+};
+
+describe('Test single commands', () => {
+  const startTest = (inputs: [string, boolean][]): void => {
+    const parser = new PaimaParser(myGrammar, parserCommands);
+    inputs.forEach((i: [string, boolean]) => {
+      test(`${i[0]} to be ${i[1]}`, () => {
+        try {
+          const value = !!parser.start(i[0]);
+          expect(value).toBe(i[1]);
+        } catch (e) {
+          expect(false).toBe(i[1]);
+        }
+      });
+    });
+  };
+
+  startTest([
+    ['j|T', false],
+    ['j|', true],
+    ['j', false],
+  ]);
+});
