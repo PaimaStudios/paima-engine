17b0600670698a51bddb102d0427a07761780863
Edward A <ac.edward@gmail.com>
2023-04-03 09:54:24 -0400

    Added support for array types

--- paima-utils-backend/test/paima-parser.array-optional.test.ts
+++ paima-utils-backend/test/paima-parser.array-optional.test.ts
@@ -2,29 +2,32 @@ import { describe, expect, test } from '@jest/globals';
 import { PaimaParser } from '../src/parser/PaimaParser';
 
 const myGrammar = `
-  arrayCommand = a|array,
-  optionalCommand = o|first?|second?|third?
+  arrayCommand = a|array?
+  arrayCommandNumber = b|array
+  arrayCommandExtra = z|text|array
 `;
 
 const parserCommands = {
   arrayCommand: {
-    // PaimaParser.ArrayParser({ item: PaimaParser.RegexParser(/[a-zA-Z]/) }),
+    array: PaimaParser.ArrayParser<string>({ item: PaimaParser.RegexParser(/[a-z0-1]/) }),
   },
-  optionalCommand: {
-    first: PaimaParser.RegexParser(/[a-zA-Z]/),
-    second: PaimaParser.RegexParser(/[a-zA-Z]/),
-    third: PaimaParser.RegexParser(/[a-zA-Z]/),
+  arrayCommandNumber: {
+    array: PaimaParser.ArrayParser<number>({ item: PaimaParser.NumberParser(0, 100) }),
+  },
+  arrayCommandExtra: {
+    text: PaimaParser.RegexParser(/[a-z]/),
+    array: PaimaParser.ArrayParser<string>({ item: PaimaParser.RegexParser(/[a-z0-1]/) }),
   },
 };
 
-describe('Test single commands', () => {
+describe('Test Array Commands', () => {
   const startTest = (inputs: [string, boolean][]): void => {
     const parser = new PaimaParser(myGrammar, parserCommands);
     inputs.forEach((i: [string, boolean]) => {
       test(`${i[0]} to be ${i[1]}`, () => {
         try {
-          const value = !!parser.start(i[0]);
-          expect(value).toBe(i[1]);
+          const value = parser.start(i[0]);
+          expect(!!value).toBe(i[1]);
         } catch (e) {
           expect(false).toBe(i[1]);
         }
@@ -37,11 +40,23 @@ describe('Test single commands', () => {
     ['a|hello,world', true],
     ['a|hello', true],
     ['a|', true],
+    ['a|0,1', true],
+    ['a|1', true],
+    ['a|2', false],
+    ['a|HELLO,WORLD', false],
+    ['a|HELLO', false],
 
-    // Optional examples
-    ['o|paima|parser|demo', true],
-    ['o|paima|parser', true],
-    ['o|', true],
-    ['o', true],
+    // Numeric Array examples
+    ['b|hello,world', false],
+    ['b|hello', false],
+    ['b|1,99', true],
+    ['b|0', true],
+    ['b|', false],
+    ['b|101', false],
+
+    // Extra
+    ['a|extra|hello,world', true],
+    ['a|extra|hello', true],
+    ['a|extra|', true],
   ]);
 });
