0c543934a598e405500e122438fdeb8914656234
Martin Tomko <martin.tomko@vacuumlabs.com>
2023-04-03 14:51:24 +0200

    Fixed CDE start blockheight calculation bug

--- paima-utils-backend/src/cde-config/utils.ts
+++ paima-utils-backend/src/cde-config/utils.ts
@@ -2,5 +2,6 @@ import type { ChainDataExtension } from '@paima/utils';
 
 export function getEarliestStartBlockheight(config: ChainDataExtension[]): number {
   const startBlockheights = config.map(cde => cde.startBlockHeight).filter(sbh => !!sbh);
-  return Math.min(0, ...startBlockheights);
+  const minStartBlockheight = Math.min(...startBlockheights);
+  return isFinite(minStartBlockheight) ? minStartBlockheight : 0;
 }
