50b32ace18bae83316ee0fca6539291d29fb8e53
Martin Tomko <martin.tomko@vacuumlabs.com>
2023-04-08 20:36:31 +0200

    Removed hardcoded CDEs from cde-config/loading

--- paima-utils-backend/src/cde-config/loading.ts
+++ paima-utils-backend/src/cde-config/loading.ts
@@ -1,33 +1,9 @@
 import * as fs from 'fs/promises';
 import YAML from 'yaml';
 
-import { ChainDataExtensionType, doLog } from '@paima/utils';
+import { doLog } from '@paima/utils';
 import type { ChainDataExtension } from '@paima/utils';
 import { parseCdeType } from './utils';
-
-const HARDCODED_CDE_DJED_SC: ChainDataExtension = {
-  cdeId: 1,
-  cdeType: ChainDataExtensionType.ERC20,
-  contractAddress: '0x69cD594C6dC452A098dCebac0eF57f445247a022',
-  startBlockHeight: 4502749,
-  initializationPrefix: '',
-};
-
-const HARDCODED_CDE_DJED_RC: ChainDataExtension = {
-  cdeId: 2,
-  cdeType: ChainDataExtensionType.ERC20,
-  contractAddress: '0x35963af2fA1E6cBf984369B69eCf24c0F1B671B2',
-  startBlockHeight: 4502749,
-  initializationPrefix: '',
-};
-
-const HARCDODED_CDE_NFT: ChainDataExtension = {
-  cdeId: 3,
-  cdeType: ChainDataExtensionType.ERC721,
-  contractAddress: '0x28d7430845044EB1A9Fc50aD7A605686CFb784DB',
-  startBlockHeight: 6228242,
-  initializationPrefix: '',
-};
 
 export async function loadChainDataExtensions(
   configFilePath: string
