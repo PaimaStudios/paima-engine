2cbe3fdf78c7efcd56c98e68c4a59a37708bf5d5
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2023-04-10 16:43:00 +0200

    Fixed validate edge case with no config

--- paima-runtime/src/cde-config/validation.ts
+++ paima-runtime/src/cde-config/validation.ts
@@ -11,18 +11,22 @@ export async function validatePersistentCdeConfig(
   smStarted: boolean
 ): Promise<boolean> {
   const persistentConfig = await getChainDataExtensions.run(undefined, DBConn);
-  if (persistentConfig.length === 0) {
-    if (smStarted) {
-      return false;
-    }
+  doLog('persistent config length:' + persistentConfig.length.toString());
 
+  // Checking cases if config is specified and if it needs to be saved
+  if (persistentConfig.length === 0 && config.length > 0) {
+    // If the node has already started syncing with no persistent config but now has a filled out yaml config
+    if (smStarted) return false;
+    // Else if it hasn't started syncing yet, store the yaml config
     return await storeCdeConfig(config, DBConn);
   }
 
+  // If the configs aren't equal in length
   if (persistentConfig.length !== config.length) {
     return false;
   }
 
+  // Verifying all CDEs match between persistent config and read yaml config
   for (const cde of config) {
     const persistent = persistentConfig.find(row => row.cde_id === cde.cdeId);
     if (!persistent) {
