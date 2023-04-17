e111fe55d5c3da5400722ea63acdd171324a95b4
Martin Tomko <martin.tomko@vacuumlabs.com>
2023-04-17 14:20:39 +0200

    Updated CDE access functions: they now take CDE name, use bigint instead of string for numeric values, and internal functionality is separate from exported functions

---
+++ paima-utils-backend/src/cde-access-internals.ts
@@ -0,0 +1,100 @@
+import type { Pool } from 'pg';
+
+import {
+  cdeErc20GetBalance,
+  cdeErc721GetOwnedNfts,
+  cdeErc721GetOwner,
+  selectChainDataExtensionsByName,
+  selectChainDataExtensionsByAddress,
+  selectChainDataExtensionsByTypeAndAddress,
+} from '@paima/db';
+import type { ChainDataExtensionType } from '@paima/utils';
+
+/* Functions to retrieve CDE ID: */
+
+export async function getCdeIdByAddress(
+  readonlyDBConn: Pool,
+  contractAddress: string
+): Promise<number | null> {
+  const results = await selectChainDataExtensionsByAddress.run(
+    { contract_address: contractAddress },
+    readonlyDBConn
+  );
+
+  if (results.length === 0) {
+    return null;
+  }
+  return results[0].cde_id;
+}
+
+export async function getCdeIdByTypeAndAddress(
+  readonlyDBConn: Pool,
+  cdeType: ChainDataExtensionType,
+  contractAddress: string
+): Promise<number | null> {
+  const results = await selectChainDataExtensionsByTypeAndAddress.run(
+    { cde_type: cdeType, contract_address: contractAddress },
+    readonlyDBConn
+  );
+  if (results.length === 0) {
+    return null;
+  }
+  return results[0].cde_id;
+}
+
+export async function getCdeIdByName(
+  readonlyDBConn: Pool,
+  cdeName: string
+): Promise<number | null> {
+  const results = await selectChainDataExtensionsByName.run({ cde_name: cdeName }, readonlyDBConn);
+  if (results.length === 0) {
+    return null;
+  }
+  return results[0].cde_id;
+}
+
+/* Functions to retrieve CDE data using the CDE ID: */
+
+export async function internalGetNftOwner(
+  readonlyDBConn: Pool,
+  cdeId: number,
+  nftId: bigint
+): Promise<string | null> {
+  const results = await cdeErc721GetOwner.run(
+    { cde_id: cdeId, token_id: nftId.toString(10) },
+    readonlyDBConn
+  );
+  if (results.length === 0) {
+    return null;
+  } else {
+    return results[0].nft_owner;
+  }
+}
+
+export async function internalGetAllOwnedNfts(
+  readonlyDBConn: Pool,
+  cdeId: number,
+  ownerAddress: string
+): Promise<bigint[]> {
+  const results = await cdeErc721GetOwnedNfts.run(
+    { cde_id: cdeId, nft_owner: ownerAddress },
+    readonlyDBConn
+  );
+  return results.map(row => BigInt(row.token_id));
+}
+
+export async function internalGetFungibleTokenBalance(
+  readonlyDBConn: Pool,
+  cdeId: number,
+  walletAddress: string
+): Promise<bigint | null> {
+  const results = await cdeErc20GetBalance.run(
+    { cde_id: cdeId, wallet_address: walletAddress },
+    readonlyDBConn
+  );
+  if (results.length === 0) {
+    return null;
+  } else {
+    return BigInt(results[0].balance);
+  }
+}
--- paima-utils-backend/src/cde-access.ts
+++ paima-utils-backend/src/cde-access.ts
@@ -1,121 +1,53 @@
 import type { Pool } from 'pg';
+
 import {
-  cdeErc20GetBalance,
-  cdeErc721GetOwnedNfts,
-  cdeErc721GetOwner,
-  selectChainDataExtensionsByAddress,
-  selectChainDataExtensionsByTypeAndAddress,
-} from '@paima/db';
-import { ChainDataExtensionType } from '@paima/utils';
-
-async function getCdeIdByAddress(
-  readonlyDBConn: Pool,
-  contractAddress: string
-): Promise<number | null> {
-  const results = await selectChainDataExtensionsByAddress.run(
-    { contract_address: contractAddress },
-    readonlyDBConn
-  );
-
-  if (results.length === 0) {
-    return null;
-  }
-  return results[0].cde_id;
-}
-
-async function getCdeIdByTypeAndAddress(
-  readonlyDBConn: Pool,
-  cdeType: ChainDataExtensionType,
-  contractAddress: string
-): Promise<number | null> {
-  const results = await selectChainDataExtensionsByTypeAndAddress.run(
-    { cde_type: cdeType, contract_address: contractAddress },
-    readonlyDBConn
-  );
-  if (results.length === 0) {
-    return null;
-  }
-  return results[0].cde_id;
-}
+  getCdeIdByName,
+  internalGetNftOwner,
+  internalGetAllOwnedNfts,
+  internalGetFungibleTokenBalance,
+} from './cde-access-internals';
 
 // Fetch the owner of the NFT from the database
 export async function getNftOwner(
   readonlyDBConn: Pool,
-  contractAddress: string,
-  nftId: string
+  cdeName: string,
+  nftId: bigint
 ): Promise<string | null> {
-  const cdeId = await getCdeIdByTypeAndAddress(
-    readonlyDBConn,
-    ChainDataExtensionType.ERC721,
-    contractAddress
-  );
-  if (cdeId == null) return null;
-
-  // Fetch owner
-  const results = await cdeErc721GetOwner.run({ cde_id: cdeId, token_id: nftId }, readonlyDBConn);
-  if (results.length === 0) {
-    return null;
-  } else {
-    return results[0].nft_owner;
-  }
+  const cdeId = await getCdeIdByName(readonlyDBConn, cdeName);
+  if (cdeId === null) return null;
+  return await internalGetNftOwner(readonlyDBConn, cdeId, nftId);
 }
 
 // Check if a given address is the owner of an nft
 export async function isNftOwner(
   readonlyDBConn: Pool,
-  contractAddress: string,
-  nftId: string,
+  cdeName: string,
+  nftId: bigint,
   ownerAddress: string
 ): Promise<boolean> {
-  const ownerRes = await getNftOwner(readonlyDBConn, contractAddress, nftId);
-  if (ownerRes && ownerRes == ownerAddress) return true;
+  const ownerRes = await getNftOwner(readonlyDBConn, cdeName, nftId);
+  if (ownerRes && ownerRes === ownerAddress) return true;
   return false;
 }
 
 // Fetch all NFTs the owner has for a given contract
 export async function getAllOwnedNfts(
   readonlyDBConn: Pool,
-  contractAddress: string,
+  cdeName: string,
   ownerAddress: string
-): Promise<string[]> {
-  const cdeId = await getCdeIdByTypeAndAddress(
-    readonlyDBConn,
-    ChainDataExtensionType.ERC721,
-    contractAddress
-  );
-  if (cdeId == null) return [];
-
-  // Fetch owned nfts
-  const results = await cdeErc721GetOwnedNfts.run(
-    { cde_id: cdeId, nft_owner: ownerAddress },
-    readonlyDBConn
-  );
-  return results.map(row => row.token_id);
+): Promise<bigint[]> {
+  const cdeId = await getCdeIdByName(readonlyDBConn, cdeName);
+  if (cdeId === null) return [];
+  return await internalGetAllOwnedNfts(readonlyDBConn, cdeId, ownerAddress);
 }
 
 // Fetch the ERC-20 balance of a given address
 export async function getFungibleTokenBalance(
   readonlyDBConn: Pool,
-  contractAddress: string,
+  cdeName: string,
   walletAddress: string
-): Promise<number | null> {
-  const cdeId = await getCdeIdByTypeAndAddress(
-    readonlyDBConn,
-    ChainDataExtensionType.ERC20,
-    contractAddress
-  );
-  if (cdeId == null) return null;
-
-  // Fetch ERC20 balance
-  const results = await cdeErc20GetBalance.run(
-    { cde_id: cdeId, wallet_address: walletAddress },
-    readonlyDBConn
-  );
-  if (results.length === 0) {
-    return null;
-  } else {
-    const balance = parseInt(results[0].balance, 10);
-    if (isNaN(balance)) return null;
-    return balance;
-  }
+): Promise<bigint | null> {
+  const cdeId = await getCdeIdByName(readonlyDBConn, cdeName);
+  if (cdeId === null) return null;
+  return await internalGetFungibleTokenBalance(readonlyDBConn, cdeId, walletAddress);
 }
