4dbca10d9009daafb012bd79e4d390241823e864
Martin Tomko <martin.tomko@vacuumlabs.com>
2023-04-27 15:00:03 +0200

    Removed all license identifiers from all Solidity files

--- contracts/nft/src/Nft.sol
+++ contracts/nft/src/Nft.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
--- contracts/nft/src/Proxy/NativeProxy.sol
+++ contracts/nft/src/Proxy/NativeProxy.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
 import "../ERC1967.sol";
