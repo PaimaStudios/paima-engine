e12ccd160621a450b8ce44d36c83a662a066db87
Martin Tomko <martin.tomko@vacuumlabs.com>
2023-05-16 17:59:38 +0200

    Updated old deployment script to save the address

--- contracts/paima-l2-contract/src/migrations/2_deploy_contracts.js
+++ contracts/paima-l2-contract/src/migrations/2_deploy_contracts.js
@@ -1,10 +1,16 @@
 const paimaL2Contract = artifacts.require("PaimaL2Contract");
 const contractConfig = require("../../truffle-config.js").contract_config;
+const utils = require("../scripts/utils.js");
+const { addAddress } = utils;
 
-module.exports = function (deployer) {
+module.exports = async function (deployer, network) {
   const {
     owner: owner,
     fee: fee
   } = contractConfig;
   deployer.deploy(paimaL2Contract, owner, fee.toString(10));
+
+  const contractInstance = await paimaL2Contract.deployed();
+  const contractAddress = contractInstance.address;
+  addAddress(network, "PaimaL2Contract", contractAddress);
 };
