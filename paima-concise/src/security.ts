995219138f85db71a3ddaa2f545e1af560d25263
Edward A <ac.edward@gmail.com>
2023-06-28 12:22:14 -0400

    lint

--- paima-concise/src/security.ts
+++ paima-concise/src/security.ts
@@ -3,16 +3,18 @@ import { separator } from './v1/consts';
 
 // For security, we add the .env CONCISE_SECURITY_PREFIX to the string.
 export const getSecurityPrefix = (): string => {
-    return ENV.CONCISE_SECURITY_PREFIX ? `${separator}${ENV.CONCISE_SECURITY_PREFIX}${separator}` : '';
-}
+  return ENV.CONCISE_SECURITY_PREFIX
+    ? `${separator}${ENV.CONCISE_SECURITY_PREFIX}${separator}`
+    : '';
+};
 
 export const checkSecurityPrefix = (conciseInput: string): boolean => {
   const securityPrefix = getSecurityPrefix();
-    if (securityPrefix) {
-      return conciseInput.startsWith(securityPrefix);
-    }
-    return true;
+  if (securityPrefix) {
+    return conciseInput.startsWith(securityPrefix);
   }
+  return true;
+};
 
 export const stripSecuirtyPrefix = (conciseInput: string): string => {
   const securityPrefix = getSecurityPrefix();
@@ -20,4 +22,4 @@ export const stripSecuirtyPrefix = (conciseInput: string): string => {
     return conciseInput.slice(securityPrefix.length);
   }
   return conciseInput;
-}
+};
