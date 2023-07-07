d01dd8394fc0c2f50bb367b5f673cefa6c13efa2
Lukas Miskovsky <miskovskylukas@gmail.com>
2023-07-07 11:36:46 +0200

    Add wallet to name encoding

---
+++ paima-mw-core/src/name-generation/index.ts
@@ -0,0 +1,19 @@
+import Prando from '@paima/prando';
+import type { WalletAddress } from '@paima/utils/src/types';
+import { adjectives } from './adjectives';
+import { nouns } from './nouns';
+
+// around 0.2% repetition with 10k users
+// around 1.5% repetition with 100k users
+// around 14.45% repetition with 1M users
+export const walletToName = (wallet: WalletAddress, maxCharacters = 20): string => {
+  const prando = new Prando(wallet);
+  const adjectiveLength = adjectives.length;
+  const nounLength = nouns.length;
+  while (true) {
+    const adjective = adjectives[Math.floor(prando.next() * adjectiveLength)];
+    const noun = nouns[Math.floor(prando.next() * nounLength)];
+    const result = `${adjective} ${noun}`;
+    if (result.length <= maxCharacters) return result;
+  }
+};
