0fe6067ca3231b259604557ed2ddaf38cd0b1308
Lukas Miskovsky <miskovskylukas@gmail.com>
2023-07-17 11:33:07 +0200

    Export and document wallet to name helper

--- paima-mw-core/src/name-generation/index.ts
+++ paima-mw-core/src/name-generation/index.ts
@@ -6,6 +6,10 @@ import { nouns } from './nouns';
 // around 0.2% repetition with 10k users
 // around 1.5% repetition with 100k users
 // around 14.45% repetition with 1M users
+/**
+ * @param maxCharacters maximum number of characters in the name (default: 20). Conversion might not finish if too small number is provided.
+ * @returns wallet address transformed into a pseudorandom name `Adjective Noun`
+ */
 export const walletToName = (wallet: WalletAddress, maxCharacters = 20): string => {
   const prando = new Prando(wallet);
   const adjectiveLength = adjectives.length;
