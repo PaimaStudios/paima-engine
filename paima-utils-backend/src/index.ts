b57e8627de908119012534de25cb09540831b085
Martin Tomko <martin.tomko@vacuumlabs.com>
2023-07-26 14:10:49 +0200

    Moved hashTogether function to backend utils

--- paima-utils-backend/src/index.ts
+++ paima-utils-backend/src/index.ts
@@ -1,2 +1,8 @@
+import Crypto from 'crypto';
+
 export * from './parser/PaimaParser';
 export * from './cde-access';
+
+export function hashTogether(data: string[]): string {
+  return Crypto.createHash('sha256').update(data.join()).digest('base64');
+}
