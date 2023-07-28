4b3b8f94ba9567eb5a8370097c96ab9ae73b6e88
Martin Tomko <martin.tomko@vacuumlabs.com>
2023-07-28 16:48:49 +0200

    Updated Paima standalone wrt. new funnel interface

--- paima-standalone/src/utils/input.ts
+++ paima-standalone/src/utils/input.ts
@@ -109,11 +109,12 @@ export const runPaimaEngine = async (): Promise<void> => {
     doLog(`Starting Game Node...`);
     doLog(`Using RPC: ${ENV.CHAIN_URI}`);
     doLog(`Targeting Smart Contact: ${ENV.CONTRACT_ADDRESS}`);
+    const stateMachine = gameSM();
     const chainFunnel: ChainFunnel = await paimaFunnel.initialize(
       ENV.CHAIN_URI,
-      ENV.CONTRACT_ADDRESS
+      ENV.CONTRACT_ADDRESS,
+      stateMachine.getReadWriteDbConn()
     );
-    const stateMachine = gameSM();
     const engine = paimaRuntime.initialize(chainFunnel, stateMachine, ENV.GAME_NODE_VERSION);
     const registerEndpoints = importTsoaFunction();
 
