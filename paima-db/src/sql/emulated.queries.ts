1820050a25fe71f73f623dd679f1b89780a2fc7a
Martin Tomko <martin.tomko@vacuumlabs.com>
2023-08-02 14:17:13 +0200

    Added new queries needed for emulated blockheights

--- paima-db/src/sql/emulated.queries.ts
+++ paima-db/src/sql/emulated.queries.ts
@@ -33,23 +33,23 @@ const emulatedSelectLatestPriorIR: any = {"usedParamSet":{"emulated_block_height
 export const emulatedSelectLatestPrior = new PreparedQuery<IEmulatedSelectLatestPriorParams,IEmulatedSelectLatestPriorResult>(emulatedSelectLatestPriorIR);
 
 
-/** 'EmulatedStoreBlockHeight' parameters type */
-export interface IEmulatedStoreBlockHeightParams {
+/** 'UpsertEmulatedBlockheight' parameters type */
+export interface IUpsertEmulatedBlockheightParams {
   deployment_chain_block_height: number;
   emulated_block_height: number;
   second_timestamp: string;
 }
 
-/** 'EmulatedStoreBlockHeight' return type */
-export type IEmulatedStoreBlockHeightResult = void;
+/** 'UpsertEmulatedBlockheight' return type */
+export type IUpsertEmulatedBlockheightResult = void;
 
-/** 'EmulatedStoreBlockHeight' query type */
-export interface IEmulatedStoreBlockHeightQuery {
-  params: IEmulatedStoreBlockHeightParams;
-  result: IEmulatedStoreBlockHeightResult;
+/** 'UpsertEmulatedBlockheight' query type */
+export interface IUpsertEmulatedBlockheightQuery {
+  params: IUpsertEmulatedBlockheightParams;
+  result: IUpsertEmulatedBlockheightResult;
 }
 
-const emulatedStoreBlockHeightIR: any = {"usedParamSet":{"deployment_chain_block_height":true,"second_timestamp":true,"emulated_block_height":true},"params":[{"name":"deployment_chain_block_height","required":true,"transform":{"type":"scalar"},"locs":[{"a":134,"b":164}]},{"name":"second_timestamp","required":true,"transform":{"type":"scalar"},"locs":[{"a":171,"b":188}]},{"name":"emulated_block_height","required":true,"transform":{"type":"scalar"},"locs":[{"a":195,"b":217}]}],"statement":"INSERT INTO emulated_block_heights(\n    deployment_chain_block_height,\n    second_timestamp,\n    emulated_block_height\n) VALUES (\n    :deployment_chain_block_height!,\n    :second_timestamp!,\n    :emulated_block_height!\n) ON CONFLICT (deployment_chain_block_height)\nDO UPDATE SET\ndeployment_chain_block_height = EXCLUDED.deployment_chain_block_height,\nsecond_timestamp = EXCLUDED.second_timestamp,\nemulated_block_height = EXCLUDED.emulated_block_height"};
+const upsertEmulatedBlockheightIR: any = {"usedParamSet":{"deployment_chain_block_height":true,"second_timestamp":true,"emulated_block_height":true},"params":[{"name":"deployment_chain_block_height","required":true,"transform":{"type":"scalar"},"locs":[{"a":134,"b":164}]},{"name":"second_timestamp","required":true,"transform":{"type":"scalar"},"locs":[{"a":171,"b":188}]},{"name":"emulated_block_height","required":true,"transform":{"type":"scalar"},"locs":[{"a":195,"b":217}]}],"statement":"INSERT INTO emulated_block_heights(\n    deployment_chain_block_height,\n    second_timestamp,\n    emulated_block_height\n) VALUES (\n    :deployment_chain_block_height!,\n    :second_timestamp!,\n    :emulated_block_height!\n) ON CONFLICT (deployment_chain_block_height)\nDO UPDATE SET\ndeployment_chain_block_height = EXCLUDED.deployment_chain_block_height,\nsecond_timestamp = EXCLUDED.second_timestamp,\nemulated_block_height = EXCLUDED.emulated_block_height"};
 
 /**
  * Query generated from SQL:
@@ -69,6 +69,62 @@ const emulatedStoreBlockHeightIR: any = {"usedParamSet":{"deployment_chain_block
  * emulated_block_height = EXCLUDED.emulated_block_height
  * ```
  */
-export const emulatedStoreBlockHeight = new PreparedQuery<IEmulatedStoreBlockHeightParams,IEmulatedStoreBlockHeightResult>(emulatedStoreBlockHeightIR);
+export const upsertEmulatedBlockheight = new PreparedQuery<IUpsertEmulatedBlockheightParams,IUpsertEmulatedBlockheightResult>(upsertEmulatedBlockheightIR);
+
+
+/** 'DeploymentChainBlockheightToEmulated' parameters type */
+export interface IDeploymentChainBlockheightToEmulatedParams {
+  deployment_chain_block_height: number;
+}
+
+/** 'DeploymentChainBlockheightToEmulated' return type */
+export interface IDeploymentChainBlockheightToEmulatedResult {
+  emulated_block_height: number;
+}
+
+/** 'DeploymentChainBlockheightToEmulated' query type */
+export interface IDeploymentChainBlockheightToEmulatedQuery {
+  params: IDeploymentChainBlockheightToEmulatedParams;
+  result: IDeploymentChainBlockheightToEmulatedResult;
+}
+
+const deploymentChainBlockheightToEmulatedIR: any = {"usedParamSet":{"deployment_chain_block_height":true},"params":[{"name":"deployment_chain_block_height","required":true,"transform":{"type":"scalar"},"locs":[{"a":95,"b":125}]}],"statement":"SELECT emulated_block_height FROM emulated_block_heights\nWHERE deployment_chain_block_height = :deployment_chain_block_height!"};
+
+/**
+ * Query generated from SQL:
+ * ```
+ * SELECT emulated_block_height FROM emulated_block_heights
+ * WHERE deployment_chain_block_height = :deployment_chain_block_height!
+ * ```
+ */
+export const deploymentChainBlockheightToEmulated = new PreparedQuery<IDeploymentChainBlockheightToEmulatedParams,IDeploymentChainBlockheightToEmulatedResult>(deploymentChainBlockheightToEmulatedIR);
+
+
+/** 'EmulatedBlockheightToDeploymentChain' parameters type */
+export interface IEmulatedBlockheightToDeploymentChainParams {
+  emulated_block_height: number;
+}
+
+/** 'EmulatedBlockheightToDeploymentChain' return type */
+export interface IEmulatedBlockheightToDeploymentChainResult {
+  deployment_chain_block_height: number;
+}
+
+/** 'EmulatedBlockheightToDeploymentChain' query type */
+export interface IEmulatedBlockheightToDeploymentChainQuery {
+  params: IEmulatedBlockheightToDeploymentChainParams;
+  result: IEmulatedBlockheightToDeploymentChainResult;
+}
+
+const emulatedBlockheightToDeploymentChainIR: any = {"usedParamSet":{"emulated_block_height":true},"params":[{"name":"emulated_block_height","required":true,"transform":{"type":"scalar"},"locs":[{"a":95,"b":117}]}],"statement":"SELECT deployment_chain_block_height FROM emulated_block_heights\nWHERE emulated_block_height = :emulated_block_height!"};
+
+/**
+ * Query generated from SQL:
+ * ```
+ * SELECT deployment_chain_block_height FROM emulated_block_heights
+ * WHERE emulated_block_height = :emulated_block_height!
+ * ```
+ */
+export const emulatedBlockheightToDeploymentChain = new PreparedQuery<IEmulatedBlockheightToDeploymentChainParams,IEmulatedBlockheightToDeploymentChainResult>(emulatedBlockheightToDeploymentChainIR);
 
 
--- paima-db/src/sql/emulated.sql
+++ paima-db/src/sql/emulated.sql
@@ -4,7 +4,7 @@ WHERE emulated_block_height <= :emulated_block_height!
 ORDER BY deployment_chain_block_height DESC
 LIMIT 1;
 
-/* @name emulatedStoreBlockHeight */
+/* @name upsertEmulatedBlockheight */
 INSERT INTO emulated_block_heights(
     deployment_chain_block_height,
     second_timestamp,
@@ -18,3 +18,11 @@ DO UPDATE SET
 deployment_chain_block_height = EXCLUDED.deployment_chain_block_height,
 second_timestamp = EXCLUDED.second_timestamp,
 emulated_block_height = EXCLUDED.emulated_block_height;
+
+/* @name deploymentChainBlockheightToEmulated */
+SELECT emulated_block_height FROM emulated_block_heights
+WHERE deployment_chain_block_height = :deployment_chain_block_height!;
+
+/* @name emulatedBlockheightToDeploymentChain */
+SELECT deployment_chain_block_height FROM emulated_block_heights
+WHERE emulated_block_height = :emulated_block_height!;
