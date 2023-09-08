e4d9a1f06c26072e9021f98600b138c79e40f69f
Sebastien Guillemot <sebastiengllmt@gmail.com>
2023-09-08 16:17:17 +0900

    Fix ci wipe command

--- wipe.sh
+++ wipe.sh
@@ -1,4 +1,8 @@
 rm -rf node_modules */node_modules */*/node_modules
 rm -rf */build
 rm -f tsconfig.tsbuildinfo */tsconfig.tsbuildinfo
-rm -f package-lock.json */package-lock.json
+
+# Don't always want to delete package-lock (especially for CI)
+if [[ "$1" != "keep-package-lock" ]]; then
+    rm -f package-lock.json */package-lock.json
+fi
