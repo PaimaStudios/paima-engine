e21d32cb9d3cf3e1e367759fd1e8bfc1f5850447
Sebastien Guillemot <sebastiengllmt@gmail.com>
2023-09-29 22:02:29 +0900

    Improve reference correctness check

--- tools/scripts/check-implicit-dependencies.ts
+++ tools/scripts/check-implicit-dependencies.ts
@@ -1,7 +1,8 @@
-const { createProjectGraphAsync } = require('@nx/workspace/src/core/project-graph');
-const fs = require('fs');
-const JSON5 = require('json5');
-const path = require('path');
+const { createProjectGraphAsync } =
+  require('@nx/workspace/src/core/project-graph') as typeof import('@nx/workspace/src/core/project-graph');
+const fs = require('fs') as typeof import('fs');
+const JSON5 = require('json5') as typeof import('json5');
+const path = require('path') as typeof import('path');
 
 /**
  * NPM requires us that all packages properly references each other in package.json
@@ -23,8 +24,20 @@ type DepAndPath = {
   dep: string;
 };
 
-const libFolders = ['paima-sdk'];
+const packageFolders = fs
+  .readdirSync('./packages', { withFileTypes: true })
+  .filter(dirent => dirent.isDirectory())
+  .map(dirent => dirent.name);
+function isPackage(path: string): boolean {
+  for (const pkg of packageFolders) {
+    if (path.includes(`packages/${pkg}`)) {
+      return true;
+    }
+  }
+  return false;
+}
 
+const libFolders = ['paima-sdk'];
 function isLib(path: string): boolean {
   for (const lib of libFolders) {
     if (path.includes(`packages/${lib}`)) {
@@ -38,7 +51,7 @@ let hasError = false;
 async function main() {
   const graph = await createProjectGraphAsync();
 
-  const ownPkgs = Object.keys(graph.nodes).filter(key => isLib(graph.nodes[key].data.root));
+  const ownPkgs = Object.keys(graph.nodes).filter(key => isPackage(graph.nodes[key].data.root));
   const ownPkgSet = new Set(ownPkgs);
 
   // cache the content of each package so we can look it up faster later
@@ -55,6 +68,7 @@ async function main() {
 
   for (const pkg of ownPkgs) {
     const pkgJson = packageContent[pkg];
+    const ownRoot = graph.nodes[pkg].data.root;
 
     const analyzedDeps = new Set(
       (graph.dependencies[pkg] as Dep[]).map(dep => dep.target).filter(dep => ownPkgSet.has(dep))
@@ -85,26 +99,31 @@ async function main() {
       }
       if (mismatches.length > 0) {
         hasError = true;
-        console.error(`Package ${pkg} has some version mismatches`);
+        console.error(
+          `Package ${purpleText(`${ownRoot}/package.json`)} has some version mismatches`
+        );
         for (const mismatch of mismatches) {
-          console.log(`- "${mismatch.name}": ${mismatch.used}"`);
-          console.log(`+ "${mismatch.name}": ${mismatch.expected}"`);
+          console.log(redText(`- "${mismatch.name}": ${mismatch.used}"`));
+          console.log(greenText(`+ "${mismatch.name}": ${mismatch.expected}"`));
         }
         console.log();
       }
     }
 
     // 2) Check that packages are present
-    {
+    //    Note: adding to package.json is only required if this is a lib we intend to publish
+    if (isLib(pkg)) {
       const remainingDeps = new Set(analyzedDeps);
       for (const declaredDep of Object.keys(declaredDeps)) {
         remainingDeps.delete(declaredDep);
       }
       if (remainingDeps.size > 0) {
         hasError = true;
-        console.error(`Package ${pkg} is missing some dependencies`);
+        console.error(
+          `Package ${purpleText(`${ownRoot}/package.json`)} is missing some dependencies`
+        );
         for (const dep of Array.from(remainingDeps)) {
-          console.error(`"${dep}": "${packageContent[dep].version}",`);
+          console.error(greenText(`"${dep}": "${packageContent[dep].version}",`));
         }
         console.log();
       }
@@ -117,11 +136,14 @@ async function main() {
         dep,
       }));
 
+      // console.log(`=== ${pkg} ===`);
+      // console.log(depAndPaths);
+
       const extra: string[] = []; // references that aren't necessary
       const wrongPath: { pkg: string; path: string }[] = [];
 
       // allow require to support trailing commas (used in tsconfig.json)
-      const jsonString = fs.readFileSync(`./${graph.nodes[pkg].data.root}/tsconfig.json`, 'utf-8');
+      const jsonString = fs.readFileSync(`./${ownRoot}/tsconfig.json`, 'utf-8');
       const tsconfigJson = JSON5.parse(jsonString);
       for (const ref of tsconfigJson['references'] ?? []) {
         const { path } = ref;
@@ -129,11 +151,13 @@ async function main() {
         const indexOfMatch = depAndPaths.findIndex(depthAndPath =>
           depthAndPath.path.endsWith(folder)
         );
+        // console.log(folder, indexOfMatch);
         if (indexOfMatch === -1) {
           extra.push(folder);
         } else {
           const fileRef = getFilenameFromPath(path);
-          if (hasBuildConfig[pkg]) {
+          // console.log(fileRef, pkg, hasBuildConfig[depAndPaths[indexOfMatch].dep]);
+          if (hasBuildConfig[depAndPaths[indexOfMatch].dep]) {
             if (fileRef !== 'tsconfig.build.json') {
               wrongPath.push({ pkg, path: path });
               hasError = true;
@@ -144,25 +168,39 @@ async function main() {
       }
       if (wrongPath.length > 0) {
         hasError = true;
-        console.error(`Package ${pkg} tsconfig.json has some incorrect references`);
+        console.error(
+          `Package ${purpleText(`${ownRoot}/tsconfig.json`)} has some incorrect references`
+        );
         for (const path of wrongPath) {
-          console.error(`${path.path} → ${path.path}/tsconfig.build.json`);
+          console.error(`${redText(path.path)} → ${greenText(`${path.path}/tsconfig.build.json`)}`);
         }
         console.log();
       }
       if (extra.length > 0) {
         hasError = true;
-        console.error(`Package ${pkg} tsconfig.json has unnecessary references`);
+        console.error(
+          `Package ${purpleText(`${ownRoot}/tsconfig.json`)} has unnecessary references`
+        );
         for (const ref of extra) {
-          console.error(ref);
+          console.error(redText(ref));
         }
         console.log();
       }
       if (depAndPaths.length > 0) {
         hasError = true;
-        console.error(`Package ${pkg} tsconfig.json is missing some references`);
-        for (const dep of depAndPaths) {
-          console.error(dep.dep);
+        console.error(
+          `Package ${purpleText(`${ownRoot}/tsconfig.json`)} is missing some references`
+        );
+        for (const depAndPath of depAndPaths) {
+          const basePath = path.relative(ownRoot, depAndPath.path);
+          hasBuildConfig[depAndPath.dep];
+          console.error(
+            greenText(
+              `{ "path": "${
+                hasBuildConfig[depAndPath.dep] ? `${basePath}/tsconfig.build.json` : basePath
+              }" },`
+            )
+          );
         }
         console.log();
       }
@@ -189,24 +227,13 @@ function getMeaningfulPartOfPath(inputPath: string) {
     pathParts.pop();
   }
 
-  return pathParts.join(path.sep);
-}
+  const result = pathParts.join(path.sep);
 
-function getFileForPath(inputPath: string) {
-  const normalizedPath = path.normalize(inputPath);
-  const pathParts = normalizedPath.split(path.sep);
-
-  // Remove '..' and '.' from the beginning
-  while (pathParts.length && (pathParts[0] === '..' || pathParts[0] === '.')) {
-    pathParts.shift();
+  // normalize by removing any trailing slash
+  if (result.endsWith('/')) {
+    return result.slice(0, -1);
   }
-
-  // Remove the filename
-  if (pathParts.length && path.extname(pathParts[pathParts.length - 1]) !== '') {
-    pathParts.pop();
-  }
-
-  return pathParts.join(path.sep);
+  return result;
 }
 
 function getFilenameFromPath(filePath: string): string | null {
@@ -215,5 +242,9 @@ function getFilenameFromPath(filePath: string): string | null {
   // obviously files in Linux don't require a file extension, but tsconfig.json always does so it's okay
   return baseName && baseName.includes('.') ? baseName : null;
 }
+
+const greenText = (text: string) => `\x1b[32m${text}\x1b[0m`;
+const redText = (text: string) => `\x1b[31m${text}\x1b[0m`;
+const purpleText = (text: string) => `\x1b[35m${text}\x1b[0m`;
 
 void main();
