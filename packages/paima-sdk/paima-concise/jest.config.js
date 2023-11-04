55daf4bc45622c254a7a7bc1347325ec7ab0161e
Sebastien <sebastiengllmt@gmail.com>
2023-11-04 10:32:49 +0400

    Remove gitignore hiding paima-concise test config

---
+++ packages/paima-sdk/paima-concise/jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
+export default {
+  displayName: '@paima/concise',
+  preset: '../../../jest.preset.js',
+  coverageDirectory: '../../../coverage/packages/engine/paima-concise',
+};
