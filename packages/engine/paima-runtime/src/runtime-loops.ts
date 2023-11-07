378833866c3ab28f1d8776b438e68cc16d66575c
Enzo Cioppettini <enzo.cioppettini@globant.com>
2023-11-06 23:11:28 -0300

    use local (tmp) carp client lib in the funnel
    just to test until the up to date version is published on npm

--- packages/engine/paima-runtime/src/runtime-loops.ts
+++ packages/engine/paima-runtime/src/runtime-loops.ts
@@ -2,7 +2,9 @@ import process from 'process';
 import { doLog, logError, delay, Network, ENV } from '@paima/utils';
 import { tx, DataMigrations } from '@paima/db';
 import { getEarliestStartBlockheight, getEarliestStartSlot } from './cde-config/utils.js';
-import type { ChainFunnel, IFunnelFactory, ReadPresyncDataFrom } from './types.js';
+import type { ChainFunnel,
+  IFunnelFactory,
+  ReadPresyncDataFrom} from './types.js';
 import type { ChainData, ChainDataExtension, GameStateMachine } from '@paima/sm';
 import { run } from './run-flag.js';
 import { snapshotIfTime } from './snapshots.js';
