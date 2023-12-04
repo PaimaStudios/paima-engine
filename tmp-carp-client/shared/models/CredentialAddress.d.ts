a8f417cd93b4a513c6e730de884e1595e844ca87
Enzo Cioppettini <enzo.cioppettini@globant.com>
2023-12-04 15:10:46 -0300

    replace carp local client with 2.3.0 from npm

--- tmp-carp-client/shared/models/CredentialAddress.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Credential, Bech32FullAddress } from "./Address";
-import type { UntilBlockPagination, PageInfo } from "./common";
-export declare type CredentialAddressRequest = {
-    credentials: Credential[];
-    after?: Bech32FullAddress;
-} & UntilBlockPagination;
-export declare type CredentialAddressResponse = {
-    addresses: Bech32FullAddress[];
-} & PageInfo;
--- tmp-carp-client/shared/models/Pool.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @pattern [0-9a-fA-F]{56}
- * @example "8200581c8baf48931c5187cd59fde553f4e7da2e1a2aa9202ec6e67815cb3f8a"
- */
-export declare type PoolHex = string;
-export declare type Pool = PoolHex;
