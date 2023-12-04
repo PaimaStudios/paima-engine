06a4493ab4b5e911ec9028b92ac9d8e70a443c59
Enzo Cioppettini <enzo.cioppettini@globant.com>
2023-12-04 15:10:46 -0300

    replace carp local client with 2.3.0 from npm

--- tmp-carp-client/shared/models/AddressUsed.d.ts
+++
@@ -1,8 +0,0 @@
-import { Address } from "./Address";
-import { Pagination } from "./common";
-export declare type AddressUsedRequest = {
-    addresses: Address[];
-} & Pagination;
-export declare type AddressUsedResponse = {
-    addresses: Address[];
-};
