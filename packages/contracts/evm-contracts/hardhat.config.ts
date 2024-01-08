bf67d2874cc27713c7d1f59c927f51b95800edd9
Sebastien Guillemot <sebastiengllmt@gmail.com>
2024-01-08 17:54:04 +0900

    Solidity docgen POC

--- packages/contracts/evm-contracts/hardhat.config.ts
+++ packages/contracts/evm-contracts/hardhat.config.ts
@@ -4,12 +4,34 @@ import type { HardhatUserConfig } from 'hardhat/config';
 import 'hardhat-abi-exporter';
 // eslint-disable-next-line import/extensions
 import './build/plugin/index.js';
+import 'solidity-docgen';
+import path from 'path';
+import { SiteConfig } from 'solidity-docgen/dist/site.js';
+import fs from 'fs';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.13',
   paths: {
     artifacts: './artifacts/hardhat',
     cache: './cache/hardhat',
+  },
+  docgen: {
+    outputDir: 'artifacts/docs',
+    templates: 'docs/templates',
+    exclude: ['dev'],
+    pageExtension: '.adoc',
+    pages: (_, file, config: SiteConfig) => {
+      // For each contract file, find the closest README.adoc and return its location as the output page path.
+      // const root = (config as any).root;
+      const sourcesDir = path.resolve(config.root, config.sourcesDir);
+      let dir = path.resolve(config.root, file.absolutePath);
+      while (dir.startsWith(sourcesDir)) {
+        dir = path.dirname(dir);
+        if (fs.existsSync(path.join(dir, 'README.adoc'))) {
+          return path.relative(sourcesDir, dir) + config.pageExtension;
+        }
+      }
+    },
   },
   defaultNetwork: 'localhost',
   networks: {
