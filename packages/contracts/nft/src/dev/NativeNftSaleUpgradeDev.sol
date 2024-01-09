e50fa744c8972d28ec45fc6127dcb3622c913ffd
Sebastien Guillemot <sebastiengllmt@gmail.com>
2024-01-09 23:06:34 +0900

    Delete old contract folders

--- packages/contracts/nft/src/dev/NativeNftSaleUpgradeDev.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.13;
-
-import "../NativeNftSale.sol";
-import "./UpgradeDev.sol";
-
-/// @dev For testing upgradeability.
-contract NativeNftSaleUpgradeDev is UpgradeDev, NativeNftSale {}
