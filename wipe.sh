82e7a1ca76ef6522ce42385632e3aa65853eb9af
Sebastien Guillemot <sebastiengllmt@gmail.com>
2024-01-15 01:43:53 +0900

    Fix broken package-lock

--- wipe.sh
+++ wipe.sh
@@ -3,11 +3,15 @@
 npx nx reset # note: has to run before node_modules deletion
 find packages -maxdepth 3 -name 'tsconfig.tsbuildinfo' -type f -exec rm -f {} +
 find packages -maxdepth 3 -name 'tsconfig.*.tsbuildinfo' -type f -exec rm -f {} +
-find packages -maxdepth 3 -name 'build' -type d -exec rm -rf {} +
+find packages -maxdepth 3 -name 'build' -type d -prune -exec rm -rf {} +
 
-if [[ "$1" == "remove-package-lock" ]]; then
-    rm -f package-lock.json */package-lock.json
-elif [[ "$1" == "remove-modules" ]]; then
-    rm -f package-lock.json */package-lock.json
-    rm -rf node_modules */node_modules */*/node_modules
+# don't remove just the package-lock because of https://github.com/npm/cli/issues/6301
+# if [[ "$1" == "remove-package-lock" ]]; then
+#     rm -f package-lock.json */package-lock.json
+# elif [[ "$1" == "remove-modules" ]]; then
+if [[ "$1" == "remove-modules" ]]; then
+    rm -f package-lock.json
+    find packages -maxdepth 3 -name 'package-lock.json' -type f -exec rm -f {} +
+    rm -rf node_modules
+    find packages -maxdepth 3 -name 'node_modules' -type d -prune -exec rm -rf '{}' +
 fi
