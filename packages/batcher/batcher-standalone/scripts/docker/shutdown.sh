ed216ebc9aa136d0a3d97fa8eb34232041f93e88
Sebastien Guillemot <sebastiengllmt@gmail.com>
2024-01-20 07:09:36 +0900

    Update batcher configs

--- packages/batcher/batcher-standalone/scripts/docker/shutdown.sh
+++ packages/batcher/batcher-standalone/scripts/docker/shutdown.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-export ENV_FILE="../.env.${NETWORK:-localhost}"; docker compose --env-file $ENV_FILE down -v
+set -eu
+
+export ENV_FILE="../.env.${NETWORK:-localhost}";
+docker compose --env-file $ENV_FILE down -v
--- packages/batcher/batcher-standalone/scripts/docker/start.sh
+++ packages/batcher/batcher-standalone/scripts/docker/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eu
 
 if [[ ! -f "./paima-batcher-linux" ]]; then
     echo "Error: paima-batcher-linux binary not found. Docker requires the linux binary for the application."
@@ -7,4 +8,7 @@ fi
 
 if [ ! -x ./paima-batcher-linux ]; then chmod +x ./paima-batcher-linux; fi
 
-export ENV_FILE="../.env.${NETWORK:-localhost}"; echo \"ENV FILE: $ENV_FILE\"; docker compose --env-file $ENV_FILE build && docker compose --env-file $ENV_FILE up
+export ENV_FILE="../.env.${NETWORK:-localhost}";
+echo \"ENV FILE: $ENV_FILE\";
+docker compose --env-file $ENV_FILE build
+docker compose --env-file $ENV_FILE up
