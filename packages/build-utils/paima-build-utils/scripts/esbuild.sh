dc1db3b6d23f188d5f9576b60e1d65776ad84688
Sebastien Guillemot <sebastiengllmt@gmail.com>
2024-02-09 16:12:26 +0900

    Add warning to paiamMiddleware for non-ESM builds

--- packages/build-utils/paima-build-utils/scripts/esbuild.sh
+++ packages/build-utils/paima-build-utils/scripts/esbuild.sh
@@ -7,4 +7,5 @@ echo "Finished Packaging"
 
 echo "Vanilla Middleware (With Exports) Prepared In: packaged/middleware.js"
 
+# note: this will not work if the esbuild format is "iife" (it will create a broken file instead)
 head -n $(( $(grep -n '^export {' packaged/middleware.js | head -1 | cut -d: -f1) - 1 )) packaged/middleware.js > packaged/paimaMiddleware.js
