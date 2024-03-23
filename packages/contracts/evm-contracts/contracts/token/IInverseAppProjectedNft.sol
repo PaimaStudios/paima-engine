f4e92e692a5c36748a8bfb100b31844493610516
Sebastien Guillemot <sebastiengllmt@gmail.com>
2024-03-23 11:56:43 +0900

    Update inverse app projection to work with ZK proofs

--- packages/contracts/evm-contracts/contracts/token/IInverseAppProjectedNft.sol
+++ packages/contracts/evm-contracts/contracts/token/IInverseAppProjectedNft.sol
@@ -13,5 +13,17 @@ interface IInverseAppProjectedNft is IInverseProjectedNft {
     /// Increases the `totalSupply` and `currentTokenId`.
     /// Reverts if `_to` is a zero address or if it refers to smart contract but does not implement IERC721Receiver-onERC721Received.
     /// Emits the `Minted` event.
+    /// @param _to where to send the NFT to
+    /// @param _verificationData any additional data to verify the validity of the mint
+    function mint(address _to, bytes memory _verificationData) external returns (uint256);
+
+    /// @dev This works identically to the other function with an extra data parameter,
+    ///      except this function just sets data to "".
     function mint(address _to) external returns (uint256);
+
+    /// @notice Returns the last nonce used (or 0 if the user has never minted)
+    /// @dev Useful if you need to either needs to
+    ///      1. Check if the nonce matches the expected value, or if more NFTs need to be minted
+    ///      2. Use a nonce algorithm where the next nonce depends on the current nonce
+    function currentNonce(address seller) external view returns (uint256);
 }
