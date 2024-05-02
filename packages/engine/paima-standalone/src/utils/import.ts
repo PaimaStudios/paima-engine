68103adea9749b372a0f26cde051afb884585f67
Tad Hardesty <tad@dcspark.io>
2024-05-02 15:02:27 -0700

    Improve import.ts documentation

--- packages/engine/paima-standalone/src/utils/import.ts
+++ packages/engine/paima-standalone/src/utils/import.ts
@@ -1,35 +1,48 @@
+/**
+ * Canonical definition of what Paima Engine directly imports from the
+ * `packaged/` directory. The game code itself may load what it will, or
+ * configuration may also refer to file paths within `packaged/`.
+ */
 import type { GameStateTransitionFunctionRouter } from '@paima/sm';
 import type { TsoaFunction } from '@paima/runtime';
 
-function importFile<T>(file: string): T {
+function importFile<T>(path: string): T {
   // dynamic import cannot be used here due to PKG limitations
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const { default: defaultExport } = require(`${process.cwd()}/${file}`);
-
-  return defaultExport;
+  return require(`${process.cwd}/${path}`);
 }
 
+export interface GameCodeImport {
+  default: GameStateTransitionFunctionRouter;
+}
 export const ROUTER_FILENAME = 'packaged/gameCode.cjs';
 /**
  * Reads repackaged user's code placed next to the executable in `gameCode.cjs` file
  */
-export const importGameStateTransitionRouter = (): GameStateTransitionFunctionRouter =>
-  importFile<GameStateTransitionFunctionRouter>(ROUTER_FILENAME);
+export function importGameStateTransitionRouter(): GameStateTransitionFunctionRouter {
+  return importFile<GameCodeImport>(ROUTER_FILENAME).default;
+}
 
+export interface EndpointsImport {
+  default: TsoaFunction;
+}
 export const API_FILENAME = 'packaged/endpoints.cjs';
 /**
  * Reads repackaged user's code placed next to the executable in `endpoints.cjs` file
  */
-export const importTsoaFunction = (): TsoaFunction => importFile<TsoaFunction>(API_FILENAME);
+export function importTsoaFunction(): TsoaFunction {
+  return importFile<EndpointsImport>(API_FILENAME).default;
+}
 
+export type OpenApiImport = object;
 export const GAME_OPENAPI_FILENAME = 'packaged/openapi.json';
 /**
- * Reads repackaged user's code placed next to the executable in `endpoints.cjs` file
+ * Reads OpenAPI definitions placed next to the executable in `openapi.json` file
  */
-export const importOpenApiJson = (): undefined | object => {
+export function importOpenApiJson(): OpenApiImport | undefined {
   try {
-    return require(`${process.cwd()}/${GAME_OPENAPI_FILENAME}`);
+    return importFile(GAME_OPENAPI_FILENAME);
   } catch (e) {
     return undefined;
   }
-};
+}
