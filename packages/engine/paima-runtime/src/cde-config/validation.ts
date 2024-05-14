e79812c64a91f579f4e7c14edb03b6f46970c8d1
Enzo Cioppettini <48031343+ecioppettini@users.noreply.github.com>
2024-05-13 21:08:55 -0300

    ignore dynamic primitives on file validation

--- packages/engine/paima-runtime/src/cde-config/validation.ts
+++ packages/engine/paima-runtime/src/cde-config/validation.ts
@@ -14,7 +14,10 @@ export async function validatePersistentCdeConfig(
   DBConn: Pool,
   smStarted: boolean
 ): Promise<boolean> {
-  const persistentConfig = await getChainDataExtensions.run(undefined, DBConn);
+  const persistentConfig = (await getChainDataExtensions.run(undefined, DBConn)).filter(
+    // filter dynamic primitives
+    ext => !!ext.cde_hash
+  );
 
   // Checking cases if config is specified and if it needs to be saved
   if (persistentConfig.length === 0 && config.length > 0) {
