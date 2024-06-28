15538f0b1aa72db34febe1e0ad7081728755503a
Enzo Cioppettini <48031343+ecioppettini@users.noreply.github.com>
2024-06-28 20:06:23 -0300

    handle both chains in getBlockNumber helper

--- packages/paima-sdk/paima-mw-core/src/helpers/general.ts
+++ packages/paima-sdk/paima-mw-core/src/helpers/general.ts
@@ -1,7 +1,30 @@
+import { ConfigNetworkType, GlobalConfig } from '@paima/utils';
 import { getWeb3 } from '../state.js';
+import assertNever from 'assert-never';
 
 export async function getBlockNumber(): Promise<number> {
-  return await getWeb3().then(web3 => web3.eth.getBlockNumber());
+  const config = await GlobalConfig.mainConfig();
+  const networkType = config[1].type;
+  switch (networkType) {
+    case ConfigNetworkType.EVM:
+      return await getWeb3().then(web3 => web3.eth.getBlockNumber());
+    case ConfigNetworkType.AVAIL_MAIN:
+      // FIXME: this code is duplicated, but not sure what's the best common place to put it
+      const responseRaw = await fetch(`${config[1].lightClient}/v2/status`);
+
+      if (responseRaw.status !== 200) {
+        throw new Error("Couldn't get light client status");
+      }
+
+      const response: { blocks: { available: { first: number; last: number } } } =
+        await responseRaw.json();
+
+      const last = response.blocks.available.last;
+
+      return last;
+    default:
+      assertNever(networkType);
+  }
 }
 
 export async function postDataToEndpoint(uri: string, data: string): Promise<Response> {
