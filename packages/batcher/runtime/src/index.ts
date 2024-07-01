50360787f909fa7d33d0fe9be2676dd552f26d08
Edward Alvarado <ac.edward@gmail.com>
2024-07-01 15:00:38 -0400

    Batcher Server Echo

--- packages/batcher/runtime/src/index.ts
+++ packages/batcher/runtime/src/index.ts
@@ -35,6 +35,7 @@ import * as fs from 'fs';
 import { parseSecurityYaml } from '@paima/utils-backend';
 import { getRemoteBackendVersion, initMiddlewareCore } from '@paima/mw-core';
 import { MQTTBroker } from './mqtt/mqtt-broker.js';
+import { MQTTPublisher, MQTTSystemEvents } from './mqtt/mqtt-publisher.js';
 
 setLogger(s => {
   try {
@@ -115,7 +116,18 @@ const BatcherRuntime: BatcherRuntimeInitializer = {
       ): Promise<void> {
         // pass endpoints to web server and run
 
+        // if (ENV.MQTT_BROKER) {
+        // Load Server
         MQTTBroker.getServer();
+        // TODO is this necessary?
+        const wait = (n: number): Promise<void> => new Promise(resolve => setTimeout(resolve, n));
+        await wait(1000);
+        // }
+        // Listen to all system events
+        MQTTPublisher.startListener(MQTTSystemEvents.BATCHER_HASH).catch((e: any) =>
+          console.log(e)
+        );
+
         // do not await on these as they may run forever
         void Promise.all([
           startServer(pool, errorCodeToMessage, provider, getCurrentBlock).catch(e => {
