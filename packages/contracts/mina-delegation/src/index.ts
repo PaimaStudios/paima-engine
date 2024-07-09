bd77e20dcf3859d7cfe94aa7aaf5ca57e8055298
Tad Hardesty <tad@dcspark.io>
2024-07-09 16:33:56 -0700

    Re-export o1js PublicKey to solve instanceof woes

--- packages/contracts/mina-delegation/src/index.ts
+++ packages/contracts/mina-delegation/src/index.ts
@@ -1,3 +1,7 @@
+// Re-export o1js types that are part of our public interface, or else stuff
+// might fail `instanceof` checks and cause problems if o1js is double-bundled.
+export { PublicKey } from 'o1js';
+// Export our actual contracts.
 export {
   DelegationOrder,
   DelegationOrderProgram,
