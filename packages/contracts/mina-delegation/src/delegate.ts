fa53b5827e069bd8d2b1260a8a007cd140005a6d
Tad Hardesty <tad@dcspark.io>
2024-07-19 16:16:14 -0700

    Make DelegationOrder prefix configurable

--- packages/contracts/mina-delegation/src/delegate.ts
+++ packages/contracts/mina-delegation/src/delegate.ts
@@ -38,61 +38,10 @@ export class Ecdsa extends createEcdsa(Secp256k1) {
   // o1js-provided fromHex is good enough
 }
 
-// Ethereum's fixed prefix.
+/** Ethereum's fixed prefix for `personal_sign` messages. **/
 const ethereumPrefix = Bytes.fromString('\x19Ethereum Signed Message:\n');
-// A prefix to distinguish this delegation order scheme from what might be
-// similar-looking messages.
-const delegationPrefix = Bytes.fromString('DELEGATE-WALLET:');
 
-/**
- * An order that a particular EVM address has signed to authorize (delegate)
- * a Mina address to act on its behalf.
- */
-export class DelegationOrder extends Struct({
-  /** Mina public key that the delegation order is issued for. */
-  target: PublicKey,
-  /** Ethereum public key that signed the delegation order. */
-  signer: Secp256k1.provable,
-}) {
-  constructor(value: { target: PublicKey; signer: Secp256k1 }) {
-    if (!(value.target instanceof PublicKey)) {
-      // Compensate for the possibility of duplicate o1js libraries that aren't
-      // `instanceof` each other, which messes up checks inside o1js. Can be
-      // caused by `npm link`ing to this package, for example.
-      value = { ...value, target: PublicKey.fromBase58((value.target as PublicKey).toBase58()) };
-    }
-    super(value);
-  }
-
-  private static _innerMessage(target: PublicKey): Bytes {
-    return Bytes.from([
-      ...delegationPrefix.bytes,
-      ...Bytes.from(encodeKey(target)).base64Encode().bytes,
-    ]);
-  }
-
-  /**
-   * Get the message for an Etherum wallet to sign, WITHOUT the Ethereum prefix.
-   * This is printable and should be passed to something like `personal_sign`.
-   */
-  static bytesToSign({ target }: { target: PublicKey }): Uint8Array {
-    // Accepts an object so you can pass just a PublicKey OR a DelegationOrder.
-    return this._innerMessage(target).toBytes();
-  }
-
-  /** Validate that the given Ethereum signature matches this order, WITH the Ethereum prefix. */
-  assertSignatureMatches(signature: Ecdsa) {
-    const inner = DelegationOrder._innerMessage(this.target);
-    const fullMessage = Bytes.from([
-      ...ethereumPrefix.bytes,
-      // NOTE: `inner.length` is effectively a constant so it's okay to bake it in.
-      ...Bytes.fromString(String(inner.length)).bytes,
-      ...inner.bytes,
-    ]);
-    signature.verifyV2(fullMessage, this.signer).assertTrue();
-  }
-}
-
+/** Pack 254 bits of key's X and 1 bit of isOdd into 32 bytes. */
 function encodeKey(k: PublicKey): UInt8[] {
   const bytes = [];
   const bits = [...k.x.toBits(254), k.isOdd];
@@ -110,28 +59,103 @@ function boolToU8(bool: Bool): UInt8 {
   return UInt8.from(bool.toField());
 }
 
-// ----------------------------------------------------------------------------
-// The provable program itself
-
 /**
- * A simple {@link ZkProgram} that proves that a valid signature exists for an
- * input {@link DelegationOrder}.
+ * Prepare a ZkProgram that verifies an EVM signature delegating to a Mina
+ * address under a specific prefix. The prefix will be seen by the user when
+ * signing the message and should clearly indicate the limited scope of the
+ * validity of the signature.
+ *
+ * @param prefix A prefix to distinguish these delegation orders from those for other systems.
+ * @returns An o1js Struct, ZkProgram, and Proof tied to that prefix.
+ *
+ * @example
+ * const { DelegationOrder, DelegationOrderProgram, DelegationOrderProof } =
+ *   delegateEvmToMina('Click & Moo login: ');
  */
-export const DelegationOrderProgram = ZkProgram({
-  name: 'DelegationOrderProgram',
+export function delegateEvmToMina(prefix: string) {
+  // ----------------------------------------------------------------------------
+  // Per-prefix data types
+  const delegationPrefix = Bytes.fromString(prefix);
 
-  publicInput: DelegationOrder,
+  class DelegationOrder extends Struct({
+    /** Mina public key that the delegation order is issued for. */
+    target: PublicKey,
+    /** Ethereum public key that signed the delegation order. */
+    signer: Secp256k1.provable,
+  }) {
+    constructor(value: { target: PublicKey; signer: Secp256k1 }) {
+      if (!(value.target instanceof PublicKey)) {
+        // Compensate for the possibility of duplicate o1js libraries that aren't
+        // `instanceof` each other, which messes up checks inside o1js. Can be
+        // caused by `npm link`ing to this package, for example.
+        value = { ...value, target: PublicKey.fromBase58((value.target as PublicKey).toBase58()) };
+      }
+      super(value);
+    }
 
-  methods: {
-    sign: {
-      privateInputs: [Ecdsa.provable],
+    static #_innerMessage(target: PublicKey): Bytes {
+      return Bytes.from([
+        ...delegationPrefix.bytes,
+        // Base64-encode encodeKey()
+        ...Bytes.from(encodeKey(target)).base64Encode().bytes,
+      ]);
+    }
 
-      async method(order: DelegationOrder, signature: Ecdsa) {
-        order.assertSignatureMatches(signature);
+    /**
+     * Get the message for an Etherum wallet to sign, WITHOUT the Ethereum prefix.
+     * This is printable and should be passed to something like `personal_sign`.
+     */
+    static bytesToSign({ target }: { target: PublicKey }): Uint8Array {
+      // Accepts an object so you can pass just a PublicKey OR a DelegationOrder.
+      return this.#_innerMessage(target).toBytes();
+    }
+
+    /** Validate that the given Ethereum signature matches this order, WITH the Ethereum prefix. */
+    assertSignatureMatches(signature: Ecdsa) {
+      const inner = DelegationOrder.#_innerMessage(this.target);
+      const fullMessage = Bytes.from([
+        ...ethereumPrefix.bytes,
+        // NOTE: `inner.length` is effectively a constant so it's okay to bake it in.
+        ...Bytes.fromString(String(inner.length)).bytes,
+        ...inner.bytes,
+      ]);
+      signature.verifyV2(fullMessage, this.signer).assertTrue();
+    }
+  }
+
+  // ----------------------------------------------------------------------------
+  // The provable program itself
+
+  const DelegationOrderProgram = ZkProgram({
+    name: `${prefix}DelegationOrderProgram`,
+
+    publicInput: DelegationOrder,
+
+    methods: {
+      sign: {
+        privateInputs: [Ecdsa.provable],
+
+        async method(order: DelegationOrder, signature: Ecdsa) {
+          order.assertSignatureMatches(signature);
+        },
       },
     },
-  },
-});
+  });
 
-/** A verifiable proof of {@link DelegationOrderProgram}'s success. */
-export class DelegationOrderProof extends ZkProgram.Proof(DelegationOrderProgram) {}
+  class DelegationOrderProof extends ZkProgram.Proof(DelegationOrderProgram) {}
+
+  return {
+    /**
+     * An order that a particular EVM address has signed to authorize (delegate)
+     * a Mina address to act on its behalf.
+     */
+    DelegationOrder,
+    /**
+     * A simple {@link ZkProgram} that proves that a valid signature exists for an
+     * input {@link DelegationOrder}.
+     */
+    DelegationOrderProgram,
+    /** A verifiable proof of {@link DelegationOrderProgram}'s success. */
+    DelegationOrderProof,
+  };
+}
--- packages/contracts/mina-delegation/src/index.ts
+++ packages/contracts/mina-delegation/src/index.ts
@@ -1,11 +1,1 @@
-// Re-export o1js types that are part of our public interface, or else stuff
-// might fail `instanceof` checks and cause problems if o1js is double-bundled.
-export { PublicKey } from 'o1js';
-// Export our actual contracts.
-export {
-  DelegationOrder,
-  DelegationOrderProgram,
-  DelegationOrderProof,
-  Ecdsa,
-  Secp256k1,
-} from './delegate.js';
+export { delegateEvmToMina, Ecdsa, Secp256k1 } from './delegate.js';
