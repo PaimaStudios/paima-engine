628ad0c8b20534a9ccfc112001e6c7de4a1833d2
Sebastien Guillemot <sebastiengllmt@gmail.com>
2024-07-25 16:02:08 +0900

    Restrict event publishing just to localhost

--- packages/node-sdk/paima-broker/src/event-broker.ts
+++ packages/node-sdk/paima-broker/src/event-broker.ts
@@ -2,6 +2,20 @@
 import Aedes from 'aedes';
 import type { Server } from 'aedes-server-factory';
 import { createServer } from 'aedes-server-factory';
+import ip from 'ip';
+
+function isLocalhost(ipAddress: string): boolean {
+  // note: this only detects simple cases (ex: you're not mapping different hostnames to localhost)
+  const localhostIPv4Range = ip.cidrSubnet('127.0.0.0/8');
+  const localhostIPv6Range = ip.cidrSubnet('::1/128');
+  const ipv4MappedIPv6LocalhostRange = ip.cidrSubnet('::ffff:127.0.0.0/104');
+
+  return (
+    localhostIPv4Range.contains(ipAddress) ||
+    localhostIPv6Range.contains(ipAddress) ||
+    ipv4MappedIPv6LocalhostRange.contains(ipAddress)
+  );
+}
 
 /*
  * This class implements a local MQTT Broker.
@@ -19,6 +33,33 @@ export class PaimaEventBroker {
     if (PaimaEventBroker.server) return PaimaEventBroker.server;
 
     PaimaEventBroker.aedes = new Aedes();
+    PaimaEventBroker.aedes.authorizePublish = (client, packet, callback) => {
+      if (client?.req?.socket?.remoteAddress == null)
+        return callback(
+          new Error('Error: no remove address found for  PaimaEventBroker connection')
+        );
+
+      /**
+       * to avoid players injecting commands into the Paima Engine node
+       * ex: publishing node/block/200 to try and make the node think block 200 got created
+       * we only allow receiving messages that come from localhost
+       *
+       * recall: Paima Engine node is just one client connecting to this broker
+       * note: there is no good way to different other localhost process from the Paima Engine node
+       *       as they are all just localhost processes connecting to this broker
+       *       so this localhost check could be stricter, but this should be sufficient
+       */
+      if (isLocalhost(client?.req?.socket?.remoteAddress)) {
+        return callback(null);
+      }
+      // for some reason, mqtt-cli requires this topic
+      // https://github.com/Anasnew99/mqtt-cli/blob/29ba08e66da397bdab16723c93b59ecb51d2da3e/src/index.ts#L158
+      if (packet.topic === '/ping_req_sys') {
+        return callback(null);
+      }
+      callback(new Error('Messages must come from localhost'));
+    };
+
     PaimaEventBroker.server = createServer(PaimaEventBroker.aedes, { ws: true });
 
     // WEBSOCKET PROTOCOL SERVER
