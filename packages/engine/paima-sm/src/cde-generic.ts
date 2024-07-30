e4d13e54473627aacfacae4e12d5313265698d4c
Enzo Cioppettini <48031343+ecioppettini@users.noreply.github.com>
2024-07-30 19:25:05 -0300

    fix: generic primitives are not scheduled on presync

--- packages/engine/paima-sm/src/cde-generic.ts
+++ packages/engine/paima-sm/src/cde-generic.ts
@@ -17,18 +17,17 @@ export default async function processDatum(
   const prefix = cdeDatum.scheduledPrefix;
 
   const scheduledBlockHeight = inPresync ? ENV.SM_START_BLOCKHEIGHT + 1 : cdeDatum.blockNumber;
+
   const stringifiedPayload = JSON.stringify(payload);
   const scheduledInputData = `${prefix}|${stringifiedPayload}`;
 
-  const updateList: SQLUpdate[] = inPresync
-    ? []
-    : [
-        createScheduledData(scheduledInputData, scheduledBlockHeight, {
-          cdeName: cdeDatum.cdeName,
-          txHash: cdeDatum.transactionHash,
-          network: cdeDatum.network,
-        }),
-      ];
+  const updateList: SQLUpdate[] = [
+    createScheduledData(scheduledInputData, scheduledBlockHeight, {
+      cdeName: cdeDatum.cdeName,
+      txHash: cdeDatum.transactionHash,
+      network: cdeDatum.network,
+    }),
+  ];
 
   updateList.push([
     cdeGenericInsertData,
