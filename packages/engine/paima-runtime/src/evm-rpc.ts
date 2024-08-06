e6dd5abfef9422541676fb28a02be729d6db0794
Sebastien Guillemot <sebastiengllmt@gmail.com>
2024-08-06 17:09:58 +0900

    Implement cache

--- packages/engine/paima-runtime/src/evm-rpc.ts
+++ packages/engine/paima-runtime/src/evm-rpc.ts
@@ -1,4 +1,5 @@
 import { JsonRpcEngine, createAsyncMiddleware } from '@metamask/json-rpc-engine';
+import type { JsonRpcParams, JsonRpcRequest } from '@metamask/utils';
 import {
   InternalRpcError,
   MethodNotFoundRpcError,
@@ -11,24 +12,79 @@ import { ENV } from '@paima/utils';
 
 export const evmRpcEngine = new JsonRpcEngine();
 
-// TODO: implement cache
-function isCached(req: any): boolean {
-  return false;
-}
-function insertIntoCache(res: any, cb: any): void {
-  return;
-}
-function getResultFromCache(req: any): any {
-  return;
+/**
+ * ==============
+ * Cache handling
+ * ==============
+ */
+
+/**
+ * Any JSON-compatible value.
+ */
+export type Json =
+  | null
+  | boolean
+  | number
+  | string
+  | Json[]
+  | {
+      [prop: string]: Json;
+    };
+
+/** turn the RPC request into a string for easy comparison. We want to do this only once and reuse it */
+type StringifiedJsonRpcRequest = string;
+type CacheKey = StringifiedJsonRpcRequest;
+type CacheValue = { val: undefined | Json; time: number };
+type RequestCache = Record<CacheKey, CacheValue>;
+
+const requestCache: RequestCache = {};
+
+/**
+ * Cache for half a block instead of a full block
+ * Otherwise, if the cache is close to the block boundary, you could be missing entire blocks
+ *
+ * ex:
+ *     - assume we're starting at block A
+ *     - assume BLOCK_TIME = 10
+ *     cache A occurs at timestamp 0
+ *     block B happens at timestamp 1
+ *     cache A invalidated at timestamp 10
+ *     block C happens at timestamp 11
+ * In this example, there was only a very small interval where B data was actually queryable since it was quickly replaced with C
+ */
+const cacheLength = (ENV.BLOCK_TIME * 1000) / 2;
+
+function insertIntoCache(req: CacheKey, val: undefined | Json): void {
+  requestCache[req] = {
+    val,
+    time: new Date().getTime(),
+  };
 }
 
+function cacheCleanup(): void {
+  const currTime = new Date().getTime();
+  for (const key of Object.keys(requestCache)) {
+    if (requestCache[key].time < currTime - cacheLength) {
+      delete requestCache[key];
+    }
+  }
+}
+setInterval(cacheCleanup, cacheLength);
+
+function getResultFromCache(req: CacheKey): undefined | CacheValue {
+  return requestCache[req];
+}
+
+// cache middleware
 evmRpcEngine.push(
   createAsyncMiddleware(async (req, res, next) => {
-    if (!isCached(req)) {
-      await next();
-      insertIntoCache(req, res);
+    const stringifiedReq = JSON.stringify(req);
+    const cacheEntry = getResultFromCache(stringifiedReq);
+    if (cacheEntry == null) {
+      await next(); // note: rely on this `next` call to fill res.result
+      insertIntoCache(stringifiedReq, res.result);
     } else {
-      res.result = getResultFromCache(req);
+      res.result = cacheEntry.val;
     }
   })
 );
@@ -37,6 +93,12 @@ type EvmRpcReturn<Method extends string> = Extract<
   PublicRpcSchema[number],
   { Method: Method }
 >['ReturnType'];
+
+/**
+ * ===============================
+ * JSON-RPC Middleware definitions
+ * ===============================
+ */
 
 evmRpcEngine.push(
   createAsyncMiddleware(async (req, res, next) => {
