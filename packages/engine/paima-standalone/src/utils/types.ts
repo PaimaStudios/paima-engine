eb86714938a1e58ba627a173281fe9023ca74b79
Enzo Cioppettini <48031343+ecioppettini@users.noreply.github.com>
2024-09-10 02:28:40 -0300

    remove paima-standalone

--- packages/engine/paima-standalone/src/utils/types.ts
+++
@@ -1,9 +0,0 @@
-const templates = [
-  'generic',
-  'chess',
-  'rock-paper-scissors',
-  'open-world',
-  'nft-lvlup',
-  'web-2.5',
-] as const;
-export type Template = (typeof templates)[number];
